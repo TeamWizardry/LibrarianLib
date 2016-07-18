c8eb8d7e0bbd1f1a4b8a386d5ebf19ecee0b7028
Pierce Corcoran <pierce@plasticcow.com>
2016-07-17 19:09:23 -0500

    Wind and friction

--- src/main/java/com/teamwizardry/librarianlib/ragdoll/cloth/Cloth.java
+++ src/main/java/com/teamwizardry/librarianlib/ragdoll/cloth/Cloth.java
@@ -19,7 +19,7 @@ public class Cloth {
 	public Vec3d[] top;
 	public int height;
 	public Vec3d size;
-	public float stretch = 1, shear = 1, flex = 0.8f, air = 0.2f;
+	public float stretch = 1, shear = 1, flex = 1f, air = 0.2f;
 	
 	public Cloth(Vec3d[] top, int height, Vec3d size) {
 		this.top = top;
@@ -58,7 +58,11 @@ public class Cloth {
 					links.add(new Link(masses[x][z], masses[x+1][z+1], shear));
 				if(x+1 < masses.length && z-1 >= 0)
 					links.add(new Link(masses[x][z], masses[x+1][z-1], shear));
-				
+			}
+		}
+		
+		for (int x = 0; x < masses.length; x++) {
+			for (int z = 0; z < masses[x].length; z++) {
 				if(x+2 < masses.length) {
 					float dist = (float)(
 							masses[x  ][z].pos.subtract(masses[x+1][z].pos).lengthVector() +
@@ -73,15 +77,32 @@ public class Cloth {
 						); // even if initialized bent, try to keep flat.
 					links.add(new Link(masses[x][z], masses[x][z+2], dist, flex));
 				}
+				
+				if(x+2 < masses.length && z+2 < masses[x].length) {
+					float dist = (float)(
+							masses[x  ][z  ].pos.subtract(masses[x+1][z+1].pos).lengthVector() +
+							masses[x+1][z+1].pos.subtract(masses[x+2][z+2].pos).lengthVector()
+						); // even if initialized bent, try to keep flat.
+					links.add(new Link(masses[x][z], masses[x+2][z+2], dist, flex));
+				}
+				if(x+2 < masses.length && z-2 > 0) {
+					float dist = (float)(
+							masses[x  ][z  ].pos.subtract(masses[x+1][z-1].pos).lengthVector() +
+							masses[x+1][z-1].pos.subtract(masses[x+2][z-2].pos).lengthVector()
+						); // even if initialized bent, try to keep flat.
+					links.add(new Link(masses[x][z], masses[x+2][z-2], dist, flex));
+				}
 			}
 		}
 	}
 	
 	public void tick(List<AxisAlignedBB> aabbs) {
-		air = 1.0f;
+		air = 3.5f;
+		double friction = 0.2;
+		
 		for (int i = 0; i < aabbs.size(); i++) {
 		}
-		Vec3d gravity = new Vec3d(0, -0.1, 0);
+		Vec3d gravity = new Vec3d(0, -0.01, 0);
 
 		for (PointMass3D[] column : masses) {
             for (PointMass3D point : column) {
@@ -96,10 +117,10 @@ public class Cloth {
                 if(point.pin)
 					continue;
                 Vec3d lastMotion = point.pos.subtract(point.prevPos);
-				point.pos = point.pos.add(gravity); // gravity
-				point.pos.add(lastMotion); // existing motion
+				point.applyMotion(lastMotion); // existing motion
+				point.applyForce(gravity); // gravity
 				
-				Vec3d wind = lastMotion.add(new Vec3d(0.0, 0.0, 10.0));
+				Vec3d wind = lastMotion.add(new Vec3d(0.0, 0.0, 1.0/20.0));
 				Vec3d normal = Vec3d.ZERO;
 				
 				if(x > 0 && y > 0) {
@@ -119,10 +140,17 @@ public class Cloth {
 				}
 				
 				normal = normal.normalize();
-				wind = wind.scale(wind.lengthVector());
-				Vec3d force = normal.scale(wind.dotProduct(normal));
 				
-				point.pos.add(force.scale(-air));//normal.scale(-air).scale(Math.pow(wind.lengthVector(), 2))); // air resistance
+				double angle = Math.acos(wind.normalize().dotProduct(normal));
+				
+				// https://books.google.com/books?id=x5cLAQAAIAAJ&pg=PA5&lpg=PA5&dq=wind+pressure+on+a+flat+angled+surface&source=bl&ots=g090hiOfxv&sig=MqZQhLMozsMNndJtkA1R_bk5KiA&hl=en&sa=X&ved=0ahUKEwiozMW2z_vNAhUD7yYKHeqvBVcQ6AEILjAC#v=onepage&q&f=false
+				// page 5-6. I'm using formula (5)
+				// wind vector length squared is flat pressure. All the other terms can be changed in the air coefficent.
+				Vec3d force = normal.scale(( Math.pow(wind.lengthVector(), 2) * angle )/(Math.PI/4));
+				
+				point.applyForce(force.scale(air));
+					
+				point.friction = null;
 			}
 		}
 		
@@ -139,32 +167,20 @@ public class Cloth {
         for (PointMass3D[] column : masses) {
             for (PointMass3D point : column) {
                 if(!point.pin) {
-//					point.origPos = point.origPos.subtract(point.pos.subtract(point.origPos).scale(0.1));
-					for (AxisAlignedBB aabb : aabbs) {
-						Vec3d vecA = point.origPos, vecB = point.pos;
-						Vec3d res = null;
-						if(vecA.yCoord > vecB.yCoord) {
-				            res = collideWithYPlane(aabb, aabb.maxY, vecA, vecB);
-				    	}
-				    	
-				    	if(vecA.yCoord < vecB.yCoord) {
-				            res = collideWithYPlane(aabb, aabb.minY, vecA, vecB);
-				    	}
-				    	
+                	point.friction = null;
+                	for (AxisAlignedBB aabb : aabbs) {
+						Vec3d res = calculateIntercept(aabb, point, true);
 						if(res != null) {
-							double f = 0.8;
-							point.pos = new Vec3d(vecB.xCoord - (vecB.xCoord-res.xCoord)*f, res.yCoord, vecB.zCoord - (vecB.zCoord-res.zCoord)*f);
-						}
-					}
-					for (AxisAlignedBB aabb : aabbs) {
-						Vec3d res = calculateIntercept(aabb, point.origPos, point.pos);
-						if(res != null) {
-							if(point.origPos.yCoord == 85 && res.yCoord < 85) {
-								point.pos = calculateIntercept(aabb, point.origPos, point.pos);
-							}
 							point.pos = res;
 						}
 					}
+					for (AxisAlignedBB aabb : aabbs) {
+						Vec3d res = calculateIntercept(aabb, point, false);
+						if(res != null) {
+							point.pos = res;
+						}
+					}
+					point.applyMotion(point.friction == null ? Vec3d.ZERO : point.friction.scale(-friction));
 				}
 				point.prevPos = point.pos;
 			}
@@ -180,47 +196,52 @@ public class Cloth {
 		}
 	}
 	
-    public Vec3d calculateIntercept(AxisAlignedBB aabb, Vec3d vecA, Vec3d vecB)
+    public Vec3d calculateIntercept(AxisAlignedBB aabb, PointMass3D point, boolean yOnly)
     {
+    	Vec3d vecA = point.origPos, vecB = point.pos;
+    	
     	Vec3d vecX = null, vecY = null, vecZ = null;
     	
-    	double f = 0.8;
-    	double m = 0.0;
-    	 
     	if(vecA.yCoord > vecB.yCoord) {
-            vecY = collideWithYPlane(aabb, aabb.maxY, vecA.add(new Vec3d(0, m, 0)), vecB);
+            vecY = collideWithYPlane(aabb, aabb.maxY, vecA, vecB);
     	}
     	
     	if(vecA.yCoord < vecB.yCoord) {
-            vecY = collideWithYPlane(aabb, aabb.minY, vecA.add(new Vec3d(0, -m, 0)), vecB);
+            vecY = collideWithYPlane(aabb, aabb.minY, vecA, vecB);
     	}
     	
     	if(vecY != null) {
-    		return new Vec3d(vecB.xCoord - (vecB.xCoord-vecY.xCoord)*f, vecY.yCoord, vecB.zCoord - (vecB.zCoord-vecY.zCoord)*f);
+			point.friction = new Vec3d(vecB.xCoord-vecY.xCoord, 0, vecB.zCoord-vecY.zCoord);
+    		return new Vec3d(vecB.xCoord, vecY.yCoord, vecB.zCoord);
     	}
     	
+    	if(yOnly)
+    		return null;
+    	
     	if(vecA.xCoord > vecB.xCoord) {
-            vecX = collideWithXPlane(aabb, aabb.maxX, vecA.add(new Vec3d(m, 0, 0)), vecB);
+            vecX = collideWithXPlane(aabb, aabb.maxX, vecA, vecB);
     	}
     	
     	if(vecA.xCoord < vecB.xCoord) {
-            vecX = collideWithXPlane(aabb, aabb.minX, vecA.add(new Vec3d(-m, 0, 0)), vecB);
+            vecX = collideWithXPlane(aabb, aabb.minX, vecA, vecB);
     	}
     	
     	if(vecX != null) {
-            return new Vec3d(vecX.xCoord, vecB.yCoord - (vecB.yCoord-vecX.yCoord)*f, vecB.zCoord - (vecB.zCoord-vecX.zCoord)*f);
+			point.friction = new Vec3d(0, vecB.yCoord-vecX.yCoord, vecB.zCoord-vecX.zCoord);
+            return new Vec3d(vecX.xCoord, vecB.yCoord, vecB.zCoord);
     	}
     	
     	if(vecA.zCoord > vecB.zCoord) {
-            vecZ = collideWithZPlane(aabb, aabb.maxZ, vecA.add(new Vec3d(0, 0, m)), vecB);
+            vecZ = collideWithZPlane(aabb, aabb.maxZ, vecA, vecB);
     	}
     	
     	if(vecA.zCoord < vecB.zCoord) {
-            vecZ = collideWithZPlane(aabb, aabb.minZ, vecA.add(new Vec3d(0, 0, -m)), vecB);
+            vecZ = collideWithZPlane(aabb, aabb.minZ, vecA, vecB);
     	}
     	
     	if(vecZ != null) {
-    		return new Vec3d(vecB.xCoord - (vecB.xCoord-vecZ.xCoord)*f, vecB.yCoord - (vecB.yCoord-vecZ.yCoord)*f, vecZ.zCoord);
+			point.friction = new Vec3d(vecB.xCoord-vecZ.xCoord, vecB.yCoord-vecZ.yCoord, 0);
+    		return new Vec3d(vecB.xCoord, vecB.yCoord, vecZ.zCoord);
     	}
     	
     	return null;
@@ -266,14 +287,14 @@ public class Cloth {
     @VisibleForTesting
     public boolean intersectsWithYZ(AxisAlignedBB aabb, Vec3d vec)
     {
-    	double m = -0.001;
+    	double m = -0.0;
         return vec.yCoord > aabb.minY+m && vec.yCoord < aabb.maxY-m && vec.zCoord > aabb.minZ+m && vec.zCoord < aabb.maxZ-m;
     }
 
     @VisibleForTesting
     public boolean intersectsWithXZ(AxisAlignedBB aabb, Vec3d vec)
     {
-    	double m = -0.001;
+    	double m = -0.0;
         return vec.xCoord > aabb.minX+m && vec.xCoord < aabb.maxX-m && vec.zCoord > aabb.minZ+m && vec.zCoord < aabb.maxZ-m;
     }
 
--- src/main/java/com/teamwizardry/librarianlib/ragdoll/cloth/PointMass3D.java
+++ src/main/java/com/teamwizardry/librarianlib/ragdoll/cloth/PointMass3D.java
@@ -3,12 +3,20 @@ package com.teamwizardry.librarianlib.ragdoll.cloth;
 import net.minecraft.util.math.Vec3d;
 
 public class PointMass3D {
-	public Vec3d pos, prevPos, origPos;
+	public Vec3d pos, prevPos, origPos, friction;
 	public float mass;
 	public boolean pin = false;
 
 	public PointMass3D(Vec3d pos, float mass) {
 		this.prevPos = this.pos = pos;
 		this.mass = mass;
+	}
+	
+	public void applyForce(Vec3d force) {
+		pos = pos.add(force.scale(1.0/mass));
+	}
+	
+	public void applyMotion(Vec3d motion) {
+		pos = pos.add(motion);
 	}
 }
