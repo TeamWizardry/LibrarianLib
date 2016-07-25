5cbc8434dacf539b84189653a7bd70d552c335cc
LordSaad <saadodi44@gmail.com>
2016-07-25 21:19:10 +0300

    further particle improvs [checkpoint]

--- src/main/java/com/teamwizardry/librarianlib/math/shapes/Helix.java
+++ src/main/java/com/teamwizardry/librarianlib/math/shapes/Helix.java
@@ -67,7 +67,7 @@ public class Helix implements IShape3D {
      * @param curve   The stiffness of the curvature per strand
      * @param reverse Will draw the helix upside down starting from a fat bottom to a peak and vice versa
      */
-    public Helix(Vec3d center, int points, float radius, int height, int strands, float curve, boolean reverse) {
+    public Helix(Vec3d center, int points, float radius, float height, int strands, float curve, boolean reverse) {
         this.center = center;
         this.points = points;
         this.radius = radius;
@@ -87,7 +87,7 @@ public class Helix implements IShape3D {
      * @param strands The number of strands that the helix will have
      * @param rotation Will define the amount of rotations the helix will assume. Higher values will increase the amount of spins
      */
-    public Helix(Vec3d center, int points, float radius, int height, int strands, double rotation) {
+    public Helix(Vec3d center, int points, float radius, float height, int strands, double rotation) {
         this.center = center;
         this.points = points;
         this.radius = radius;
@@ -112,16 +112,45 @@ public class Helix implements IShape3D {
                     if (reverse) y -= (center.yCoord - center.yCoord + height) / points;
                     else y += (center.yCoord - center.yCoord + height) / points;
                     locs.add(center.add(new Vec3d(x, y, z)));
-                    locs.add(center.subtract(new Vec3d(x, y, z)));
+                    //locs.add(center.subtract(new Vec3d(x, y, z)));
                 }
             } else {
                 for (double y = 0; y < height; y += (center.yCoord - center.yCoord + height) / points) {
                     double x = radius * Math.cos(y * rotation);
                     double z = radius * Math.sin(y * rotation);
                     locs.add(center.add(new Vec3d(x, y, z)));
-                    locs.add(center.subtract(new Vec3d(x, y, z)));
+                    //locs.add(center.subtract(new Vec3d(x, y, z)));
                 }
             }
+        }
+        if (reverse) Collections.reverse(locs);
+        return locs;
+    }
+
+    public ArrayList<ArrayList<Vec3d>> getMultipleStrandPoints() {
+        ArrayList<ArrayList<Vec3d>> locs = new ArrayList<>();
+        for (int strand = 1; strand <= strands; strand++) {
+            ArrayList<Vec3d> strandPoints = new ArrayList<>();
+            if (shrink) {
+                double y = 0;
+                if (reverse) y = height;
+                for (int point = 1; point <= points; point++) {
+                    float ratio = (float) point / points;
+                    double angle = curve * ratio * 2 * Math.PI / strands + (2 * Math.PI * strand / strands) + rotation;
+                    double x = Math.cos(angle) * ratio * radius;
+                    double z = Math.sin(angle) * ratio * radius;
+                    if (reverse) y -= (center.yCoord - center.yCoord + height) / points;
+                    else y += (center.yCoord - center.yCoord + height) / points;
+                    strandPoints.add(center.add(new Vec3d(x, y, z)));
+                }
+            } else {
+                for (double y = 0; y < height; y += (center.yCoord - center.yCoord + height) / points) {
+                    double x = radius * Math.cos(y * rotation);
+                    double z = radius * Math.sin(y * rotation);
+                    strandPoints.add(center.add(new Vec3d(x, y, z)));
+                }
+            }
+            locs.add(strandPoints);
         }
         if (reverse) Collections.reverse(locs);
         return locs;
