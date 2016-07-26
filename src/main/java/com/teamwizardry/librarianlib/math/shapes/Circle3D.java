3e083554fb560728aa43d7bb11be1c6dd4223ec0
LordSaad <saadodi44@gmail.com>
2016-07-26 19:54:34 +0300

    better fire and adjusted some particle effects

--- src/main/java/com/teamwizardry/librarianlib/math/shapes/Circle3D.java
+++ src/main/java/com/teamwizardry/librarianlib/math/shapes/Circle3D.java
@@ -1,6 +1,7 @@
 package com.teamwizardry.librarianlib.math.shapes;
 
 import com.teamwizardry.librarianlib.api.IShape3D;
+import com.teamwizardry.librarianlib.math.Matrix4;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.ArrayList;
@@ -25,12 +26,27 @@ public class Circle3D implements IShape3D {
      */
     private double radius;
 
+    /**
+     * Orientation of the circle
+     */
+    private float pitch, yaw;
+
     private double theta = 0;
 
     public Circle3D(Vec3d origin, double radius, int particleCount) {
         this.particles = particleCount;
         this.origin = origin;
         this.radius = radius;
+        pitch = 0;
+        yaw = 0;
+    }
+
+    public Circle3D(Vec3d origin, double radius, int particleCount, float pitch, float yaw) {
+        this.particles = particleCount;
+        this.origin = origin;
+        this.radius = radius;
+        this.pitch = pitch;
+        this.yaw = yaw;
     }
 
     /**
@@ -43,9 +59,13 @@ public class Circle3D implements IShape3D {
         ArrayList<Vec3d> points = new ArrayList<>();
         for (int i = 0; i <= particles; i++) {
             double tempTheta = i * Math.toRadians(360.0 / particles);
-            double x = origin.xCoord + radius * Math.cos(tempTheta);
+            Matrix4 matrix = new Matrix4();
+            matrix.rotate(pitch, new Vec3d(0, -1, 0));
+            matrix.rotate(yaw, new Vec3d(1, 0, 0));
+            double x = origin.xCoord + radius * Math.sin(tempTheta);
             double y = origin.yCoord;
-            double z = origin.zCoord + radius * Math.sin(tempTheta);
+            double z = origin.zCoord + radius * Math.cos(tempTheta);
+            matrix.apply(new Vec3d(x, y, z));
             points.add(new Vec3d(x, y, z));
         }
         return points;
