463a24f24ad68e0bbade6f78cbeacd4490a431a1
LordSaad <saadodi44@gmail.com>
2016-08-04 15:08:07 +0300

    liblib's shader option default to false to prevent jvm crashes

--- src/main/java/com/teamwizardry/librarianlib/common/Config.java
+++ src/main/java/com/teamwizardry/librarianlib/common/Config.java
@@ -1,6 +1,5 @@
 package com.teamwizardry.librarianlib.common;
 
-import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.common.config.Configuration;
 
 import java.io.File;
@@ -11,7 +10,7 @@ public class Config {
     public static void initConfig(File configurationFile) {
         Configuration config = new Configuration(configurationFile);
         config.load();
-        shaders = config.get(Configuration.CATEGORY_CLIENT, "shaders", true, "Controls whether LibLib's shaders are used. If you're using the GLSL Shaders mod and are having graphical troubles with visuals, you may want to turn this off.").getBoolean();
+        shaders = config.get(Configuration.CATEGORY_CLIENT, "shaders", false, "Controls whether LibLib's shaders are used. If you're using the GLSL Shaders mod and are having graphical troubles with visuals, you may want to turn this off.").getBoolean();
         config.save();
     }
 
