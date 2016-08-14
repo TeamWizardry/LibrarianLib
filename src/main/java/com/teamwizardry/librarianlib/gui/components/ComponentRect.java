ee86006b7970d03f72dbcc200ddc029d2687cc49
Pierce Corcoran <pierce@plasticcow.com>
2016-08-14 12:46:47 -0700

    Fix rect component

--- src/main/java/com/teamwizardry/librarianlib/gui/components/ComponentRect.java
+++ src/main/java/com/teamwizardry/librarianlib/gui/components/ComponentRect.java
@@ -5,6 +5,11 @@ import com.teamwizardry.librarianlib.gui.Option;
 import com.teamwizardry.librarianlib.math.Vec2d;
 import com.teamwizardry.librarianlib.util.Color;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import org.lwjgl.opengl.GL11;
 
 /**
  * Created by TheCodeWarrior
@@ -19,6 +24,23 @@ public class ComponentRect extends GuiComponent<ComponentRect> {
 	
 	@Override
 	public void drawComponent(Vec2d mousePos, float partialTicks) {
-		Gui.drawRect(pos.xi, pos.yi, pos.xi + size.xi, pos.yi + size.yi, color.getValue(this).hexARGB());
+		
+		Tessellator tessellator = Tessellator.getInstance();
+		VertexBuffer vb = tessellator.getBuffer();
+		
+		Color c = color.getValue(this);
+		GlStateManager.pushAttrib();
+		GlStateManager.color(c.r, c.g, c.b, c.a);
+		GlStateManager.disableTexture2D();
+		GlStateManager.enableBlend();
+		
+		vb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
+		vb.pos(getPos().x,             getPos().y,             0).endVertex();
+		vb.pos(getPos().x,             getPos().y+getSize().y, 0).endVertex();
+		vb.pos(getPos().x+getSize().x, getPos().y+getSize().y, 0).endVertex();
+		vb.pos(getPos().x+getSize().x, getPos().y,             0).endVertex();
+		tessellator.draw();
+		
+		GlStateManager.popAttrib();
 	}
 }
