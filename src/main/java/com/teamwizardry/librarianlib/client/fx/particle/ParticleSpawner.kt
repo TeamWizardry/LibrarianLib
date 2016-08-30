61db4e26f3d9e2f15d3f142c5397ae7e14b91b24
Pierce Corcoran <pierce@plasticcow.com>
2016-08-30 10:06:22 -0700

    Add interp index function and make spawner use interp index util function

--- src/main/java/com/teamwizardry/librarianlib/client/fx/particle/ParticleSpawner.kt
+++ src/main/java/com/teamwizardry/librarianlib/client/fx/particle/ParticleSpawner.kt
@@ -1,6 +1,7 @@
 package com.teamwizardry.librarianlib.client.fx.particle
 
 import com.teamwizardry.librarianlib.common.util.math.interpolate.InterpFunction
+import com.teamwizardry.librarianlib.common.util.math.interpolate.InterpListGenerator
 import net.minecraft.client.Minecraft
 import net.minecraft.client.particle.Particle
 import net.minecraft.util.math.Vec3d
@@ -64,9 +65,8 @@ object ParticleSpawner {
     @JvmOverloads
     fun spawn(builder: ParticleBuilder, world: World, curve: InterpFunction<Vec3d>, particleCount: Int, travelTime: Int = 0, callback: BiConsumer<Float, ParticleBuilder> = noop) {
         val actualParticleCount = modifyParticleCount(particleCount)
-        val particleSpan = if(particleCount == 1) 0f else 1f/(particleCount-1)
-        for (i in 0..particleCount-1) {
-            val t = particleSpan*i
+
+        InterpListGenerator.getIndexList(actualParticleCount).forEach { t ->
             val tick = Math.floor(t*travelTime.toDouble()).toInt()
             callback.accept(t, builder)
             val particle = builder.build(world, curve.get(t))
--- src/main/java/com/teamwizardry/librarianlib/common/util/math/interpolate/InterpListGenerator.kt
+++ src/main/java/com/teamwizardry/librarianlib/common/util/math/interpolate/InterpListGenerator.kt
@@ -6,20 +6,25 @@ package com.teamwizardry.librarianlib.common.util.math.interpolate
 object InterpListGenerator {
 
     @JvmStatic
-    fun <T> getList(func: InterpFunction<T>, points: Int) : MutableList<T> {
+    fun <T> getList(func: InterpFunction<T>, points: Int) : List<T> {
+        return getIndexList(points).map { func.get(it) }
+    }
+
+    @JvmStatic
+    fun getIndexList(points: Int) : List<Float> {
         if(points <= 0)
             return mutableListOf()
 
-        var list = mutableListOf<T>()
+        var list = mutableListOf<Float>()
 
         if(points == 1) {
-            list.add(func.get(0.5f))
+            list.add(0.5f)
         } else {
             val pointDist = 1f / (points-1)
             var currentCoord = 0f
 
             while (currentCoord <= 1) {
-                list.add(func.get(currentCoord))
+                list.add(currentCoord)
                 currentCoord += pointDist
             }
         }
