9752ed8675e791e9bc1511e0e6161f06571cc5dc
yrsegal <yrsegal@gmail.com>
2016-11-06 18:25:43 -0500

    limit targets for registers

--- src/main/java/com/teamwizardry/librarianlib/common/util/autoregister/RegisterAnnotations.kt
+++ src/main/java/com/teamwizardry/librarianlib/common/util/autoregister/RegisterAnnotations.kt
@@ -4,6 +4,16 @@ package com.teamwizardry.librarianlib.common.util.autoregister
  * Created by TheCodeWarrior
  */
 
+/**
+ * todo
+ */
+@Target(AnnotationTarget.CLASS)
+@MustBeDocumented
 annotation class TileRegister(val id: String)
 
+/**
+ * todo
+ */
+@Target(AnnotationTarget.CLASS)
+@MustBeDocumented
 annotation class PartRegister(val id: String)
