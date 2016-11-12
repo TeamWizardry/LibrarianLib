0d65dacedc9decd307c125ff132d1e332cbac390
yrsegal <yrsegal@gmail.com>
2016-11-12 16:23:50 -0500

    notify of undefined behavior caveat

--- src/main/java/com/teamwizardry/librarianlib/common/util/autoregister/RegisterAnnotations.kt
+++ src/main/java/com/teamwizardry/librarianlib/common/util/autoregister/RegisterAnnotations.kt
@@ -6,6 +6,10 @@ import net.minecraft.tileentity.TileEntity;
  * Apply this to a class that extends [TileEntity] to have it be automatically registered.
  *
  * [value] is the name or domain:name to register as.
+ *
+ * Should [value] be without a prefix, the register will attempt to assign it one
+ * based on the mod jar in which it resides. If multiple mods are in the same jar,
+ * this can cause undefined behavior.
  */
 @Target(AnnotationTarget.CLASS)
 @MustBeDocumented
@@ -15,6 +19,10 @@ annotation class TileRegister(val value: String)
  * Apply this to a class that extends PartMod to have it be automatically registered.
  *
  * [value] is the name or domain:name to register as.
+ *
+ * Should [value] be without a prefix, the register will attempt to assign it one
+ * based on the mod jar in which it resides. If multiple mods are in the same jar,
+ * this can cause undefined behavior.
  */
 @Target(AnnotationTarget.CLASS)
 @MustBeDocumented
