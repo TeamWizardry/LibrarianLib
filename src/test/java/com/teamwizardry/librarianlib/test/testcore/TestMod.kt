807ddf458be392bb7fd7b787845c5dd5488628ad
yrsegal <yrsegal@gmail.com>
2016-11-12 16:43:11 -0500

    build

--- src/test/java/com/teamwizardry/librarianlib/test/testcore/TestMod.kt
+++ src/test/java/com/teamwizardry/librarianlib/test/testcore/TestMod.kt
@@ -1,11 +1,10 @@
 package com.teamwizardry.librarianlib.test.testcore
 
+import com.teamwizardry.librarianlib.common.base.ModCreativeTab
 import com.teamwizardry.librarianlib.common.core.LoggerBase
-import com.teamwizardry.librarianlib.common.util.autoregister.AutoRegisterHandler
 import com.teamwizardry.librarianlib.test.saving.SavingEntryPoint
-import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.init.Blocks
-import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.SidedProxy
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
@@ -60,9 +59,10 @@ class TestMod {
 
         lateinit var entrypoints: Array<TestEntryPoint>
 
-        val tab = object : CreativeTabs("LibLibTesting") {
-            override fun getTabIconItem() = Item.getItemFromBlock(Blocks.BOOKSHELF)
-        }
+        val tab = object : ModCreativeTab(MODID) {
+            override val iconStack: ItemStack
+                get() = ItemStack(Blocks.BOOKSHELF)
+        }.registerDefaultTab()
     }
 
 }
