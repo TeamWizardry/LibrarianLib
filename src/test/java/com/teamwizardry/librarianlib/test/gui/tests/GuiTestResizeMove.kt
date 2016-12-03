7a1e5ea9328e3108de9e5d276132dee1cc56e7b2
Pierce Corcoran <pierce@plasticcow.com>
2016-12-02 22:37:18 -0800

    Misc random changes I don't want to list. Dokka ain't working tho. I just want to reorganize and need to commit first.

--- src/test/java/com/teamwizardry/librarianlib/test/gui/tests/GuiTestResizeMove.kt
+++ src/test/java/com/teamwizardry/librarianlib/test/gui/tests/GuiTestResizeMove.kt
@@ -1,18 +1,26 @@
 package com.teamwizardry.librarianlib.test.gui.tests
 
 import com.teamwizardry.librarianlib.client.gui.GuiBase
+import com.teamwizardry.librarianlib.client.gui.GuiComponent
 import com.teamwizardry.librarianlib.client.gui.components.ComponentRect
+import com.teamwizardry.librarianlib.common.util.plus
+import com.teamwizardry.librarianlib.common.util.vec
 import java.awt.Color
 
 /**
  * Created by TheCodeWarrior
  */
-class GuiTestRect : GuiBase(100, 100) {
+class GuiTestResizeMove : GuiBase(100, 100) {
     init {
 
         val c = ComponentRect(25, 25, 50, 50)
         c.color.setValue(Color.RED)
         mainComponents.add(c)
+
+        c.BUS.hook(GuiComponent.MouseClickEvent::class.java) { e ->
+            c.pos += vec(1, 1)
+            c.size += vec(1, 0)
+        }
 
     }
 }
