66a02868b8295d1f37946358559b9c82ae1df96e
yrsegal <yrsegal@gmail.com>
2016-12-09 08:30:48 -0500

    rename config parameters to what they were before

--- src/main/java/com/teamwizardry/librarianlib/common/util/EasyConfigHandler.kt
+++ src/main/java/com/teamwizardry/librarianlib/common/util/EasyConfigHandler.kt
@@ -2,6 +2,7 @@ package com.teamwizardry.librarianlib.common.util
 
 import com.teamwizardry.librarianlib.LibrarianLib
 import com.teamwizardry.librarianlib.LibrarianLog
+import com.teamwizardry.librarianlib.common.util.EasyConfigHandler.init
 import net.minecraftforge.common.config.Configuration
 import net.minecraftforge.fml.common.discovery.ASMDataTable
 import java.io.File
@@ -63,22 +64,22 @@ object EasyConfigHandler {
         fieldMapStr.filter { it.value.getString("modid", "") == modid }.forEach {
             it.key.isAccessible = true
             if(!it.value.getBoolean("devOnly", false) || LibrarianLib.DEV_ENVIRONMENT)
-                it.key.set(null, config.get(it.value.getString("catagory", ""), it.value.getString("id", ""), it.value.getString("def", ""), it.value.getString("comment", "")).string)
+                it.key.set(null, config.get(it.value.getString("category", ""), it.value.getString("id", ""), it.value.getString("defaultValue", ""), it.value.getString("comment", "")).string)
         }
         fieldMapInt.filter { it.value.getString("modid", "") == modid }.forEach {
             it.key.isAccessible = true
             if(!it.value.getBoolean("devOnly", false) || LibrarianLib.DEV_ENVIRONMENT)
-                it.key.set(null, config.get(it.value.getString("catagory", ""), it.value.getString("id", ""), it.value.getInt("def", 0), it.value.getString("comment", "")).int)
+                it.key.set(null, config.get(it.value.getString("category", ""), it.value.getString("id", ""), it.value.getInt("defaultValue", 0), it.value.getString("comment", "")).int)
         }
         fieldMapBoolean.filter { it.value.getString("modid", "") == modid }.forEach {
             it.key.isAccessible = true
             if(!it.value.getBoolean("devOnly", false) || LibrarianLib.DEV_ENVIRONMENT)
-                it.key.set(null, config.get(it.value.getString("catagory", ""), it.value.getString("id", ""), it.value.getBoolean("def", false), it.value.getString("comment", "")).boolean)
+                it.key.set(null, config.get(it.value.getString("category", ""), it.value.getString("id", ""), it.value.getBoolean("defaultValue", false), it.value.getString("comment", "")).boolean)
         }
         fieldMapDouble.filter { it.value.getString("modid", "") == modid }.forEach {
             it.key.isAccessible = true
             if(!it.value.getBoolean("devOnly", false) || LibrarianLib.DEV_ENVIRONMENT)
-                it.key.set(null, config.get(it.value.getString("catagory", ""), it.value.getString("id", ""), it.value.getBoolean("def", false), it.value.getString("comment", "")).double)
+                it.key.set(null, config.get(it.value.getString("category", ""), it.value.getString("id", ""), it.value.getBoolean("defaultValue", false), it.value.getString("comment", "")).double)
         }
         config.save()
 
@@ -101,33 +102,33 @@ object EasyConfigHandler {
 
 /**
  * This annotation should be applied to non-final, static (if in Kotlin, [JvmStatic]) fields of type [String] (or in Kotlin String?]
- * that you wish to use as a config property. Use [catagory] to indicate the config catagory in the config file,
+ * that you wish to use as a config property. Use [category] to indicate the config category in the config file,
  * [id] will indicate the name of the property, [comment] will be the comment above the entry in the config file,
- * [def] is the default value, and if [devOnly] (optional) is set to true, this config property will only be set in a
+ * [defaultValue] is the default value, and if [devOnly] (optional) is set to true, this config property will only be set in a
  * development environment.
  */
-@Target(AnnotationTarget.FIELD) annotation class ConfigPropertyString(val modid: String, val catagory: String, val id: String, val comment: String, val def: String, val devOnly: Boolean = false)
+@Target(AnnotationTarget.FIELD) annotation class ConfigPropertyString(val modid: String, val category: String, val id: String, val comment: String, val defaultValue: String, val devOnly: Boolean = false)
 /**
  * This annotation should be applied to non-final, static (if in Kotlin, [JvmStatic]) fields of type [Int] (or in Kotlin Int?]
- * that you wish to use as a config property. Use [catagory] to indicate the config catagory in the config file,
+ * that you wish to use as a config property. Use [category] to indicate the config category in the config file,
  * [id] will indicate the name of the property, [comment] will be the comment above the entry in the config file,
- * [def] is the default value, and if [devOnly] (optional) is set to true, this config property will only be set in a
+ * [defaultValue] is the default value, and if [devOnly] (optional) is set to true, this config property will only be set in a
  * development environment.
  */
-@Target(AnnotationTarget.FIELD) annotation class ConfigPropertyInt(val modid: String, val catagory: String, val id: String, val comment: String, val def: Int, val devOnly: Boolean = false)
+@Target(AnnotationTarget.FIELD) annotation class ConfigPropertyInt(val modid: String, val category: String, val id: String, val comment: String, val defaultValue: Int, val devOnly: Boolean = false)
 /**
  * This annotation should be applied to non-final, static (if in Kotlin, [JvmStatic]) fields of type [Boolean] (or in Kotlin Boolean?]
- * that you wish to use as a config property. Use [catagory] to indicate the config catagory in the config file,
+ * that you wish to use as a config property. Use [category] to indicate the config category in the config file,
  * [id] will indicate the name of the property, [comment] will be the comment above the entry in the config file,
- * [def] is the default value, and if [devOnly] (optional) is set to true, this config property will only be set in a
+ * [defaultValue] is the default value, and if [devOnly] (optional) is set to true, this config property will only be set in a
  * development environment.
  */
-@Target(AnnotationTarget.FIELD) annotation class ConfigPropertyBoolean(val modid: String, val catagory: String, val id: String, val comment: String, val def: Boolean, val devOnly: Boolean = false)
+@Target(AnnotationTarget.FIELD) annotation class ConfigPropertyBoolean(val modid: String, val category: String, val id: String, val comment: String, val defaultValue: Boolean, val devOnly: Boolean = false)
 /**
  * This annotation should be applied to non-final, static (if in Kotlin, [JvmStatic]) fields of type [Double] (or in Kotlin Double?]
- * that you wish to use as a config property. Use [catagory] to indicate the config catagory in the config file,
+ * that you wish to use as a config property. Use [category] to indicate the config category in the config file,
  * [id] will indicate the name of the property, [comment] will be the comment above the entry in the config file,
- * [def] is the default value, and if [devOnly] (optional) is set to true, this config property will only be set in a
+ * [defaultValue] is the default value, and if [devOnly] (optional) is set to true, this config property will only be set in a
  * development environment.
  */
-@Target(AnnotationTarget.FIELD) annotation class ConfigPropertyDouble(val modid: String, val catagory: String, val id: String, val comment: String, val def: Double, val devOnly: Boolean = false)
+@Target(AnnotationTarget.FIELD) annotation class ConfigPropertyDouble(val modid: String, val category: String, val id: String, val comment: String, val defaultValue: Double, val devOnly: Boolean = false)
