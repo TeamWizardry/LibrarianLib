fbba1fad708711f7e1edff46c60d4f723015b4ef
yrsegal <yrsegal@gmail.com>
2016-12-11 21:13:44 -0500

    get rid of nonessential java

--- src/main/java/com/teamwizardry/librarianlib/common/util/event/EventBus.kt
+++ src/main/java/com/teamwizardry/librarianlib/common/util/event/EventBus.kt
@@ -1,12 +1,12 @@
 package com.teamwizardry.librarianlib.common.util.event
 
-import com.teamwizardry.librarianlib.common.util.lambdainterfs.EventHandler
+import java.util.function.Consumer
 
 /**
  * Created by TheCodeWarrior
  */
 class EventBus {
-    private val hooks = mutableMapOf<Class<*>, MutableList<EventHandler<Event>>>()
+    private val hooks = mutableMapOf<Class<*>, MutableList<Consumer<Event>>>()
 
     fun hasHooks(clazz: Class<*>): Boolean {
         return hooks[clazz]?.size ?: 0 > 0
@@ -16,25 +16,25 @@ class EventBus {
         val clazz = event.javaClass
         if (clazz in hooks) {
             if (event.reversed)
-                hooks[clazz]?.asReversed()?.forEach { hook ->
-                    hook(event)
+                hooks[clazz]?.asReversed()?.forEach {
+                    it.accept(event)
                 }
             else
-                hooks[clazz]?.forEach { hook ->
-                    hook(event)
+                hooks[clazz]?.forEach {
+                    it.accept(event)
                 }
         }
         return event
     }
 
     fun <E : Event> hook(clazz: Class<E>, hook: (E) -> Unit) {
-        hook(clazz, EventHandler(hook))
+        hook(clazz, Consumer(hook))
     }
 
     @Suppress("UNCHECKED_CAST")
-    fun <E : Event> hook(clazz: Class<E>, hook: EventHandler<E>) {
+    fun <E : Event> hook(clazz: Class<E>, hook: Consumer<E>) {
         if (!hooks.containsKey(clazz))
             hooks.put(clazz, mutableListOf())
-        hooks[clazz]?.add(hook as EventHandler<Event>)
+        hooks[clazz]?.add(hook as Consumer<Event>)
     }
 }
--- src/main/java/com/teamwizardry/librarianlib/common/util/lambdainterfs/VoidLambda.java
+++
@@ -1,9 +0,0 @@
-package com.teamwizardry.librarianlib.common.util.lambdainterfs;
-
-/**
- * Created by TheCodeWarrior
- */
-@FunctionalInterface
-public interface VoidLambda {
-    void invoke();
-}
