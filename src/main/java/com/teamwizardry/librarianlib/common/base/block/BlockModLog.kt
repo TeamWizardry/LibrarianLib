1de4a1ef40cdd8de88b2ff9ede1be8b48ef95f40
yrsegal <yrsegal@gmail.com>
2017-01-09 09:58:21 -0500

    split log into pillar and log

--- src/main/java/com/teamwizardry/librarianlib/common/base/block/BlockModLog.kt
+++ src/main/java/com/teamwizardry/librarianlib/common/base/block/BlockModLog.kt
@@ -1,23 +1,12 @@
 package com.teamwizardry.librarianlib.common.base.block
 
-import com.teamwizardry.librarianlib.client.core.JsonGenerationUtils
-import com.teamwizardry.librarianlib.client.core.ModelHandler
-import com.teamwizardry.librarianlib.common.base.IModelGenerator
-import com.teamwizardry.librarianlib.common.util.builders.json
-import net.minecraft.block.Block
-import net.minecraft.block.BlockLog
 import net.minecraft.block.SoundType
 import net.minecraft.block.material.Material
-import net.minecraft.block.properties.PropertyEnum
-import net.minecraft.block.state.BlockStateContainer
 import net.minecraft.block.state.IBlockState
-import net.minecraft.client.renderer.block.model.ModelResourceLocation
-import net.minecraft.entity.EntityLivingBase
 import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.FurnaceRecipes
 import net.minecraft.util.EnumFacing
-import net.minecraft.util.Rotation
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
@@ -28,10 +17,7 @@ import net.minecraftforge.oredict.OreDictionary
  * Created at 10:36 AM on 5/7/16.
  */
 @Suppress("LeakingThis")
-open class BlockModLog(name: String, vararg variants: String) : BlockMod(name, Material.WOOD, *variants), IModelGenerator {
-    companion object {
-        val AXIS: PropertyEnum<BlockLog.EnumAxis> = PropertyEnum.create("axis", BlockLog.EnumAxis::class.java)
-    }
+open class BlockModLog(name: String, vararg variants: String) : BlockModPillar(name, Material.WOOD, *variants) {
 
     init {
         blockHardness = 2.0f
@@ -41,7 +27,6 @@ open class BlockModLog(name: String, vararg variants: String) : BlockMod(name, M
                 OreDictionary.registerOre("logWood", ItemStack(this, 1, variant))
             FurnaceRecipes.instance().addSmeltingRecipeForBlock(this, ItemStack(Items.COAL, 1, 1), 0.15f)
         }
-        defaultState = defaultState.withProperty(AXIS, BlockLog.EnumAxis.Y)
     }
 
     override fun breakBlock(worldIn: World, pos: BlockPos, state: IBlockState) {
@@ -62,68 +47,12 @@ open class BlockModLog(name: String, vararg variants: String) : BlockMod(name, M
     override fun getFlammability(world: IBlockAccess?, pos: BlockPos?, face: EnumFacing?) = 5
     override fun getFireSpreadSpeed(world: IBlockAccess?, pos: BlockPos?, face: EnumFacing?) = 5
 
-    override fun getStateForPlacement(worldIn: World?, pos: BlockPos?, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float, meta: Int, placer: EntityLivingBase?): IBlockState {
-        return this.getStateFromMeta(meta).withProperty(AXIS, BlockLog.EnumAxis.fromFacingAxis(facing.axis))
-    }
-
-    override fun withRotation(state: IBlockState, rot: Rotation): IBlockState {
-        when (rot) {
-            Rotation.COUNTERCLOCKWISE_90, Rotation.CLOCKWISE_90 -> {
-
-                when (state.getValue(AXIS)) {
-                    BlockLog.EnumAxis.X -> return state.withProperty(AXIS, BlockLog.EnumAxis.Z)
-                    BlockLog.EnumAxis.Z -> return state.withProperty(AXIS, BlockLog.EnumAxis.X)
-                    else -> return state
-                }
-            }
-
-            else -> return state
-        }
-    }
-
     override fun canSustainLeaves(state: IBlockState?, world: IBlockAccess?, pos: BlockPos?): Boolean {
         return true
     }
 
     override fun isWood(world: IBlockAccess?, pos: BlockPos?): Boolean {
         return true
-    }
-
-    override fun rotateBlock(world: World, pos: BlockPos, axis: EnumFacing?): Boolean {
-        val state = world.getBlockState(pos)
-        world.setBlockState(pos, state.cycleProperty(AXIS))
-        return true
-    }
-
-    override fun getStateFromMeta(meta: Int): IBlockState {
-        var axis = BlockLog.EnumAxis.Y
-        val i = meta and 12
-
-        when (i) {
-            4 -> axis = BlockLog.EnumAxis.X
-            8 -> axis = BlockLog.EnumAxis.Z
-            12 -> axis = BlockLog.EnumAxis.NONE
-        }
-
-        return this.defaultState.withProperty(AXIS, axis)
-    }
-
-    override fun getMetaFromState(state: IBlockState?): Int {
-        state ?: return 0
-        var i = 0
-
-        when (state.getValue(AXIS)) {
-            BlockLog.EnumAxis.X -> i = i or 4
-            BlockLog.EnumAxis.Z -> i = i or 8
-            BlockLog.EnumAxis.NONE -> i = i or 12
-            else -> i = i or 0
-        }
-
-        return i
-    }
-
-    override fun createBlockState(): BlockStateContainer? {
-        return BlockStateContainer(this, AXIS)
     }
 
     override fun getHarvestTool(state: IBlockState?): String? {
@@ -134,47 +63,6 @@ open class BlockModLog(name: String, vararg variants: String) : BlockMod(name, M
         return type == "axe"
     }
 
-    override fun generateMissingBlockstate(mapper: ((Block) -> Map<IBlockState, ModelResourceLocation>)?): Boolean {
-        ModelHandler.generateBlockJson(this, {
-            JsonGenerationUtils.generateBlockStates(this, mapper) {
-                if ("axis=y" in it) json {
-                    obj(
-                            "model" to registryName.toString()
-                    )
-                } else if ("axis=x" in it) json {
-                    obj(
-                            "model" to registryName.toString(),
-                            "x" to 90,
-                            "y" to 90
-                    )
-                } else if ("axis=z" in it) json {
-                    obj(
-                            "model" to registryName.toString(),
-                            "x" to 90
-                    )
-                } else json {
-                    obj(
-                            "model" to registryName.toString() + "_bark"
-                    )
-                }
-            }
-        }, {
-            mapOf(*variants.flatMap {
-                listOf(
-                        JsonGenerationUtils.getPathForBlockModel(this)
-                                to json {
-                            obj(
-                                    "parent" to "block/cube_column",
-                                    "textures" to obj(
-                                            "end" to "$modId:blocks/$it",
-                                            "side" to "$modId:blocks/${it}_bark"
-                                    )
-                            )
-                        },
-                        JsonGenerationUtils.getPathForBlockModel(this, "${it}_bark")
-                                to JsonGenerationUtils.generateBaseBlockModel(this, "${it}_bark"))
-            }.toTypedArray())
-        })
-        return true
-    }
+    override val postFix: String
+        get() = "bark"
 }
---
+++ src/main/java/com/teamwizardry/librarianlib/common/base/block/BlockModPillar.kt
@@ -0,0 +1,136 @@
+package com.teamwizardry.librarianlib.common.base.block
+
+import com.teamwizardry.librarianlib.client.core.JsonGenerationUtils
+import com.teamwizardry.librarianlib.client.core.ModelHandler
+import com.teamwizardry.librarianlib.common.base.IModelGenerator
+import com.teamwizardry.librarianlib.common.util.builders.json
+import net.minecraft.block.Block
+import net.minecraft.block.BlockLog
+import net.minecraft.block.material.Material
+import net.minecraft.block.properties.PropertyEnum
+import net.minecraft.block.state.BlockStateContainer
+import net.minecraft.block.state.IBlockState
+import net.minecraft.client.renderer.block.model.ModelResourceLocation
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.Rotation
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+/**
+ * @author WireSegal
+ * Created at 10:36 AM on 5/7/16.
+ */
+@Suppress("LeakingThis")
+open class BlockModPillar(name: String, material: Material, vararg variants: String) : BlockMod(name, material, *variants), IModelGenerator {
+    companion object {
+        val AXIS: PropertyEnum<BlockLog.EnumAxis> = PropertyEnum.create("axis", BlockLog.EnumAxis::class.java)
+    }
+
+    init {
+        defaultState = defaultState.withProperty(AXIS, BlockLog.EnumAxis.Y)
+    }
+
+    override fun getStateForPlacement(worldIn: World?, pos: BlockPos?, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float, meta: Int, placer: EntityLivingBase?): IBlockState {
+        return this.getStateFromMeta(meta).withProperty(AXIS, BlockLog.EnumAxis.fromFacingAxis(facing.axis))
+    }
+
+    override fun withRotation(state: IBlockState, rot: Rotation): IBlockState {
+        when (rot) {
+            Rotation.COUNTERCLOCKWISE_90, Rotation.CLOCKWISE_90 -> {
+
+                when (state.getValue(AXIS)) {
+                    BlockLog.EnumAxis.X -> return state.withProperty(AXIS, BlockLog.EnumAxis.Z)
+                    BlockLog.EnumAxis.Z -> return state.withProperty(AXIS, BlockLog.EnumAxis.X)
+                    else -> return state
+                }
+            }
+
+            else -> return state
+        }
+    }
+
+    override fun rotateBlock(world: World, pos: BlockPos, axis: EnumFacing?): Boolean {
+        val state = world.getBlockState(pos)
+        world.setBlockState(pos, state.cycleProperty(AXIS))
+        return true
+    }
+
+    override fun getStateFromMeta(meta: Int): IBlockState {
+        var axis = BlockLog.EnumAxis.Y
+        val i = meta and 12
+
+        when (i) {
+            4 -> axis = BlockLog.EnumAxis.X
+            8 -> axis = BlockLog.EnumAxis.Z
+            12 -> axis = BlockLog.EnumAxis.NONE
+        }
+
+        return this.defaultState.withProperty(AXIS, axis)
+    }
+
+    override fun getMetaFromState(state: IBlockState?): Int {
+        state ?: return 0
+        var i = 0
+
+        when (state.getValue(AXIS)) {
+            BlockLog.EnumAxis.X -> i = i or 4
+            BlockLog.EnumAxis.Z -> i = i or 8
+            BlockLog.EnumAxis.NONE -> i = i or 12
+            else -> i = i or 0
+        }
+
+        return i
+    }
+
+    override fun createBlockState(): BlockStateContainer? {
+        return BlockStateContainer(this, AXIS)
+    }
+
+    override fun generateMissingBlockstate(mapper: ((Block) -> Map<IBlockState, ModelResourceLocation>)?): Boolean {
+        ModelHandler.generateBlockJson(this, {
+            JsonGenerationUtils.generateBlockStates(this, mapper) {
+                if ("axis=y" in it) json {
+                    obj(
+                            "model" to registryName.toString()
+                    )
+                } else if ("axis=x" in it) json {
+                    obj(
+                            "model" to registryName.toString(),
+                            "x" to 90,
+                            "y" to 90
+                    )
+                } else if ("axis=z" in it) json {
+                    obj(
+                            "model" to registryName.toString(),
+                            "x" to 90
+                    )
+                } else json {
+                    obj(
+                            "model" to registryName.toString() + "_$postFix"
+                    )
+                }
+            }
+        }, {
+            mapOf(*variants.flatMap {
+                listOf(
+                        JsonGenerationUtils.getPathForBlockModel(this)
+                                to json {
+                            obj(
+                                    "parent" to "block/cube_column",
+                                    "textures" to obj(
+                                            "end" to "$modId:blocks/$it",
+                                            "side" to "$modId:blocks/${it}_$postFix"
+                                    )
+                            )
+                        },
+                        JsonGenerationUtils.getPathForBlockModel(this, "${it}_$postFix")
+                                to JsonGenerationUtils.generateBaseBlockModel(this, "${it}_$postFix"))
+            }.toTypedArray())
+        })
+        return true
+    }
+
+    protected open val postFix: String
+        get() = "bark"
+}
