2dd597f566df38d56edacb541055f9ce273c8202
yrsegal <yrsegal@gmail.com>
2017-02-11 20:17:52 -0500

    bauble base

---
+++ src/main/java/com/teamwizardry/librarianlib/common/base/item/ItemModBauble.kt
@@ -0,0 +1,103 @@
+package com.teamwizardry.librarianlib.common.base.item
+
+import baubles.api.BaubleType
+import baubles.api.BaublesApi
+import baubles.api.IBauble
+import com.teamwizardry.librarianlib.LibrarianLib
+import com.teamwizardry.librarianlib.client.util.TooltipHelper
+import com.teamwizardry.librarianlib.common.core.LibLibSoundEvents
+import com.teamwizardry.librarianlib.common.util.ItemNBTHelper
+import net.minecraft.client.Minecraft
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.ItemStack
+import net.minecraft.util.ActionResult
+import net.minecraft.util.EnumActionResult
+import net.minecraft.util.EnumHand
+import net.minecraft.util.SoundCategory
+import net.minecraft.world.World
+import net.minecraftforge.fml.common.Optional
+
+/**
+ * @author WireSegal
+ * Created at 7:55 PM on 2/11/17.
+ */
+@Optional.Interface(iface = "baubles.api.IBauble", modid = "Baubles")
+abstract class ItemModBauble(name: String, vararg variants: String) : ItemMod(name, *variants), IBauble {
+    companion object {
+        val TAG_HASHCODE = "playerHashcode"
+
+        fun getLastPlayerHashcode(stack: ItemStack) = ItemNBTHelper.getInt(stack, TAG_HASHCODE, 0)
+        fun setLastPlayerHashcode(stack: ItemStack, hash: Int) = ItemNBTHelper.setInt(stack, TAG_HASHCODE, hash)
+    }
+
+    init {
+        maxStackSize = 1
+    }
+
+    @Optional.Method(modid = "Baubles")
+    override abstract fun getBaubleType(stack: ItemStack): BaubleType
+
+    override fun onItemRightClick(stack: ItemStack, world: World, player: EntityPlayer, hand: EnumHand): ActionResult<ItemStack> {
+        if (canEquip(stack, player)) {
+            val baubles = BaublesApi.getBaublesHandler(player) ?: return ActionResult(EnumActionResult.FAIL, stack)
+            for (i in 0..baubles.slots - 1) {
+                if (baubles.isItemValidForSlot(i, stack, player)) {
+                    val stackInSlot = baubles.getStackInSlot(i)
+                    if (stackInSlot == null || (stackInSlot.item as IBauble).canUnequip(stackInSlot, player)) {
+                        if (!world.isRemote) {
+                            baubles.setStackInSlot(i, stack.copy())
+                            if (!player.capabilities.isCreativeMode)
+                                player.inventory.setInventorySlotContents(player.inventory.currentItem, null)
+                        }
+
+                        if (stackInSlot != null) {
+                            (stackInSlot.item as IBauble).onUnequipped(stackInSlot, player)
+                            return ActionResult.newResult(EnumActionResult.SUCCESS, stackInSlot.copy())
+                        }
+                        break
+                    }
+                }
+            }
+        }
+
+        return ActionResult.newResult(EnumActionResult.PASS, stack)
+    }
+
+    open fun onEquippedOrLoadedIntoWorld(stack: ItemStack, player: EntityLivingBase) {
+        //NO-OP
+    }
+
+    @Optional.Method(modid = "Baubles")
+    override fun onEquipped(stack: ItemStack, player: EntityLivingBase) {
+        if (!player.world.isRemote)
+            player.world.playSound(null, player.posX, player.posY, player.posZ, LibLibSoundEvents.baubleEquip, SoundCategory.PLAYERS, 0.1F, 1.3F)
+
+        onEquippedOrLoadedIntoWorld(stack, player)
+        setLastPlayerHashcode(stack, player.hashCode())
+    }
+
+    @Optional.Method(modid = "Baubles")
+    override fun onWornTick(stack: ItemStack, player: EntityLivingBase) {
+        if (getLastPlayerHashcode(stack) != player.hashCode()) {
+            onEquippedOrLoadedIntoWorld(stack, player)
+            setLastPlayerHashcode(stack, player.hashCode())
+        }
+    }
+
+    @Optional.Method(modid = "Baubles")
+    override fun addInformation(stack: ItemStack, player: EntityPlayer, tooltip: MutableList<String>, advanced: Boolean) {
+        TooltipHelper.tooltipIfShift(tooltip) {
+            addHiddenTooltip(stack, player, tooltip, advanced)
+        }
+    }
+
+    open fun addHiddenTooltip(stack: ItemStack, player: EntityPlayer, tooltip: MutableList<String>, advanced: Boolean) {
+        val keys = Minecraft.getMinecraft().gameSettings.keyBindings
+        val key: String? = keys
+                .firstOrNull { it.keyDescription == "Baubles Inventory" }
+                ?.displayName
+
+        if (key != null) TooltipHelper.addToTooltip(tooltip, "${LibrarianLib.MODID}.baubleTooltip", key)
+    }
+}
---
+++ src/main/java/com/teamwizardry/librarianlib/common/core/LibLibSoundEvents.kt
@@ -0,0 +1,11 @@
+package com.teamwizardry.librarianlib.common.core
+
+import com.teamwizardry.librarianlib.common.base.ModSoundEvent
+
+/**
+ * @author WireSegal
+ * Created at 8:01 PM on 2/11/17.
+ */
+object LibLibSoundEvents {
+    val baubleEquip = ModSoundEvent("baubleEquip")
+}
