ae8f7ee191edd119ce8cde51fc7e73800221ee93
Pierce Corcoran <pierce@plasticcow.com>
2017-02-26 14:05:02 -0800

    Added custom logger formatting, for stuff like Worlds being printed as "[world name]([dim id])"

--- src/main/java/com/teamwizardry/librarianlib/common/core/LoggerBase.kt
+++ src/main/java/com/teamwizardry/librarianlib/common/core/LoggerBase.kt
@@ -7,6 +7,7 @@ import com.teamwizardry.librarianlib.common.util.toComponent
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.text.Style
 import net.minecraft.util.text.TextFormatting
+import net.minecraft.world.World
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
@@ -19,33 +20,62 @@ abstract class LoggerBase protected constructor(name: String) {
         logger = LogManager.getLogger(name)
     }
 
+    /**
+     * Performs special processing on formatting arguments, such as replacing Worlds with "WORLD_NAME (DIM_ID)"
+     */
+    open fun processFormatArg(value: Any): Any {
+
+        if(value is World) {
+            return "${value.providerName} (${value.provider.dimension})"
+        }
+
+        return value
+    }
+
+    /**
+     * Process the passed formatting args, passing each non-null element through [processFormatArg]
+     *
+     * @return a modified array
+     */
+    fun processFormatting(value: Array<out Any?>): Array<Any?> {
+        val arr = arrayOfNulls<Any?>(value.size)
+        for(i in value.indices) {
+            val v = value[i]
+            if(v != null)
+                arr[i] = processFormatArg(v)
+            else
+                arr[i] = null
+        }
+        return arr
+    }
+
     fun error(message: String, vararg args: Any?) {
-        logger.log(Level.ERROR, String.format(message, *args))
+        logger.log(Level.ERROR, String.format(message, *processFormatting(args)))
     }
 
     fun error(e: Exception, message: String, vararg args: Any?) {
-        logger.log(Level.ERROR, String.format(message, *args))
+        logger.log(Level.ERROR, String.format(message, *processFormatting(args)))
         e.printStackTrace()
     }
 
     fun warn(message: String, vararg args: Any?) {
-        logger.log(Level.WARN, String.format(message, *args))
+        logger.log(Level.WARN, String.format(message, *processFormatting(args)))
     }
 
     fun info(message: String, vararg args: Any?) {
-        logger.log(Level.INFO, String.format(message, *args))
+        logger.log(Level.INFO, String.format(message, *processFormatting(args)))
     }
 
     fun debug(message: String, vararg args: Any?) {
-        if (debugMode) logger.log(Level.INFO, String.format(message, *args))
+        if (debugMode) logger.log(Level.INFO, String.format(message, *processFormatting(args)))
     }
 
     fun message(player: EntityPlayer, message: String, vararg args: Any?) {
-        player.sendMessage(String.format(message, *args))
+        player.sendMessage(String.format(message, *processFormatting(args)))
     }
 
     fun warn(player: EntityPlayer, message: String, vararg args: Any?) {
-        player.sendStatusMessage(String.format(message, *args).toComponent().setStyle(Style().setColor(TextFormatting.RED)), false)
+        player.sendStatusMessage(String.format(message, *processFormatting(args)).toComponent().setStyle(Style().setColor(TextFormatting.RED)), false)
     }
 
     /**
