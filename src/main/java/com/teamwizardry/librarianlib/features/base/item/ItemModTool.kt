2165ab6ed81ec9df03f659646f959b51eccae106
yrsegal <yrsegal@gmail.com>
2017-04-01 20:54:52 -0400

    futher automate

--- src/main/java/com/teamwizardry/librarianlib/features/base/item/ItemModTool.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/base/item/ItemModTool.kt
@@ -20,29 +20,51 @@ import net.minecraft.util.NonNullList
  * The default implementation for an IVariantHolder tool.
  */
 @Suppress("LeakingThis")
-open class ItemModTool(name: String, attackDamage: Float, attackSpeed: Float, toolMaterial: ToolMaterial, effectiveBlocks: Set<Block>, vararg variants: String) : ItemTool(attackDamage, attackSpeed, toolMaterial, effectiveBlocks), IModItemProvider, IModelGenerator {
+open class ItemModTool(name: String, attackDamage: Float, attackSpeed: Float, toolMaterial: ToolMaterial, effectiveBlocks: Set<Block>) : ItemTool(attackDamage, attackSpeed, toolMaterial, effectiveBlocks), IModItemProvider, IModelGenerator {
 
     companion object {
         private val AXE_EFFECTIVE_ON = setOf(Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER)
         private val PICKAXE_EFFECTIVE_ON = setOf(Blocks.ACTIVATOR_RAIL, Blocks.COAL_ORE, Blocks.COBBLESTONE, Blocks.DETECTOR_RAIL, Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_ORE, Blocks.DOUBLE_STONE_SLAB, Blocks.GOLDEN_RAIL, Blocks.GOLD_BLOCK, Blocks.GOLD_ORE, Blocks.ICE, Blocks.IRON_BLOCK, Blocks.IRON_ORE, Blocks.LAPIS_BLOCK, Blocks.LAPIS_ORE, Blocks.LIT_REDSTONE_ORE, Blocks.MOSSY_COBBLESTONE, Blocks.NETHERRACK, Blocks.PACKED_ICE, Blocks.RAIL, Blocks.REDSTONE_ORE, Blocks.SANDSTONE, Blocks.RED_SANDSTONE, Blocks.STONE, Blocks.STONE_SLAB)
         private val SHOVEL_EFFECTIVE_ON = setOf(Blocks.CLAY, Blocks.DIRT, Blocks.FARMLAND, Blocks.GRASS, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.SAND, Blocks.SNOW, Blocks.SNOW_LAYER, Blocks.SOUL_SAND)
 
-        fun fromToolClass(toolClass: String) = when(toolClass) {
+        fun blocksClassIsEffectiveOn(toolClass: String) = when (toolClass) {
             "axe" -> AXE_EFFECTIVE_ON
             "shovel" -> SHOVEL_EFFECTIVE_ON
             "pickaxe" -> PICKAXE_EFFECTIVE_ON
             else -> emptySet()
+        }
+
+        fun classAttackDamage(toolClass: String, material: ToolMaterial) = when (toolClass) {
+            "axe" -> when (material.harvestLevel) {
+                0 -> 6f
+                else -> 8f
+            }
+            "shovel" -> 1.5f
+            "pickaxe" -> 1.0f
+            else -> 0f
+        }
+
+        fun classAttackSpeed(toolClass: String, material: ToolMaterial) = when (toolClass) {
+            "axe" -> if (material.efficiencyOnProperMaterial <= 5f)
+                -3.2f
+            else if (material.efficiencyOnProperMaterial <= 7f)
+                -3.1f
+            else
+                -3.0f
+            "shovel" -> 1.5f
+            "pickaxe" -> 1.0f
+            else -> 0f
         }
     }
 
     var toolClass: String? = null
         protected set
 
-    constructor(name: String, attackDamage: Float, attackSpeed: Float, toolMaterial: ToolMaterial, toolClass: String, vararg variants: String) : this(name, attackDamage, attackSpeed, toolMaterial, fromToolClass(toolClass), *variants) {
+    constructor(name: String, attackDamage: Float, attackSpeed: Float, toolMaterial: ToolMaterial, toolClass: String) : this(name, attackDamage, attackSpeed, toolMaterial, blocksClassIsEffectiveOn(toolClass)) {
         this.toolClass = toolClass
     }
-    constructor(name: String, toolMaterial: ToolMaterial, effectiveBlocks: Set<Block>, vararg variants: String) : this(name, 0F, 0F, toolMaterial, effectiveBlocks, *variants)
-    constructor(name: String, toolMaterial: ToolMaterial, toolClass: String, vararg variants: String) : this(name, 0F, 0F, toolMaterial, fromToolClass(toolClass), *variants)  {
+    constructor(name: String, toolMaterial: ToolMaterial, effectiveBlocks: Set<Block>) : this(name, 0F, 0F, toolMaterial, effectiveBlocks)
+    constructor(name: String, toolMaterial: ToolMaterial, toolClass: String) : this(name, classAttackDamage(toolClass, toolMaterial), classAttackSpeed(toolClass, toolMaterial), toolMaterial, blocksClassIsEffectiveOn(toolClass))  {
         this.toolClass = toolClass
     }
 
@@ -51,7 +73,7 @@ open class ItemModTool(name: String, attackDamage: Float, attackSpeed: Float, to
 
     private val bareName = VariantHelper.toSnakeCase(name)
     private val modId = currentModId
-    override val variants = VariantHelper.setupItem(this, bareName, variants, creativeTab)
+    override val variants = VariantHelper.setupItem(this, bareName, arrayOf(), creativeTab)
 
     override fun setUnlocalizedName(name: String): Item {
         VariantHelper.setUnlocalizedNameForItem(this, modId, name)
