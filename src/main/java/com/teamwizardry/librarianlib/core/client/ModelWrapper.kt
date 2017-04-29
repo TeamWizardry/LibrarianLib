10f7a01d6827c2ba1eb792c997807e185f51eb55
yrsegal <yrsegal@gmail.com>
2017-04-29 17:14:55 -0400

    cleanup imports

--- src/main/java/com/teamwizardry/librarianlib/core/client/ModelWrapper.kt
+++ src/main/java/com/teamwizardry/librarianlib/core/client/ModelWrapper.kt
@@ -1,17 +1,11 @@
 package com.teamwizardry.librarianlib.core.client
 
-import com.sun.tools.doclets.internal.toolkit.util.DocPath.parent
 import net.minecraft.block.state.IBlockState
-import net.minecraft.client.renderer.block.model.BakedQuad
 import net.minecraft.client.renderer.block.model.IBakedModel
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms
-import net.minecraft.client.renderer.block.model.ItemOverrideList
-import net.minecraft.client.renderer.texture.TextureAtlasSprite
 import net.minecraft.util.EnumFacing
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
 import java.util.function.IntPredicate
-import java.util.function.Predicate
 
 /**
  * @author WireSegal
