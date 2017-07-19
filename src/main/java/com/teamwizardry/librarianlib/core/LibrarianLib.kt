232aefad8b6ffbcb2377029ce16f8e70763e0eba
Pierce Corcoran <pierce@plasticcow.com>
2017-07-18 20:42:22 -0700

    Now the code _actually_ says it's 4.0

--- src/main/java/com/teamwizardry/librarianlib/core/LibrarianLib.kt
+++ src/main/java/com/teamwizardry/librarianlib/core/LibrarianLib.kt
@@ -78,7 +78,7 @@ object LibrarianLib {
 
     const val MODID = "librarianlib"
     const val MODNAME = "LibrarianLib"
-    const val VERSION = "3.1.5"
+    const val VERSION = "4.0"
     const val ALLOWED = "[1.12,)"
     const val CLIENT = "com.teamwizardry.librarianlib.core.client.LibClientProxy"
     const val SERVER = "com.teamwizardry.librarianlib.core.common.LibCommonProxy"
