5a251685f66ff34e641124dd344d9b4085283e15
Bluexin <bluexin.games@gmail.com>
2017-09-07 12:58:53 +0200

    Added some more checks to fix potential future crashes

--- src/main/java/com/teamwizardry/librarianlib/features/utilities/AnnotationHelper.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/utilities/AnnotationHelper.kt
@@ -54,6 +54,8 @@ object AnnotationHelper {
         for (data in table.getAll(annotationClass.name)) {
             try {
                 callback(Class.forName(data.className).asSubclass(superClass) as Class<out T>, AnnotationInfo(data.annotationInfo))
+            } catch (e: ClassNotFoundException) { // Class is sided and we are on wrong side
+                continue
             } catch (ex: Exception) {
                 throw ex
             }
@@ -72,7 +74,11 @@ object AnnotationHelper {
                 val index = data.objectName.indexOf('(')
 
                 if (index != -1 && data.objectName.indexOf(')') == index + 1) {
-                    val method = Class.forName(data.className).getDeclaredMethod(data.objectName.substring(0, index))
+                    val method = try {
+                        Class.forName(data.className)
+                    } catch (e: ClassNotFoundException) { // Class is sided and we are on wrong side
+                        continue
+                    }.getDeclaredMethod(data.objectName.substring(0, index))
 
                     if (method != null) {
                         method.isAccessible = true
