167ecba5bcea79d547f6d718c85752450d075660
Pierce Corcoran <pierce@plasticcow.com>
2017-10-24 12:22:31 -0700

    Fixed ScheduledEventAnimation

--- src/main/java/com/teamwizardry/librarianlib/features/animator/animations/ScheduledEventAnimation.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/animator/animations/ScheduledEventAnimation.kt
@@ -8,19 +8,20 @@ import com.teamwizardry.librarianlib.features.animator.Animation
  *
  * Created by TheCodeWarrior
  */
-class ScheduledEventAnimation(time: Float, private val callback: (time: Float) -> Unit) :
+class ScheduledEventAnimation(time: Float, private val callback: () -> Unit) :
         Animation<ScheduledEventAnimation.PointlessAnimatableObject>(PointlessAnimatableObject,
                 AnimatableProperty.get(PointlessAnimatableObject::class.java, "field")) {
+    constructor(time: Float, callback_runnable: Runnable) : this(time, { callback_runnable.run() })
     init {
         start = time
     }
 
     override fun update(time: Float) {
         if(this.timeFraction(time) == 1f)
-            callback(time)
+            callback()
     }
 
     object PointlessAnimatableObject {
-        @JvmField var field = 0
+        var field = 0
     }
 }
