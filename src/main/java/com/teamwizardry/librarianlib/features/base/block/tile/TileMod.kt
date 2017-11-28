b64aaaddfa31e3294410d032a20def4499b27e3f
yrsegal <yrsegal@gmail.com>
2017-11-27 21:21:30 -0500

    please

--- src/main/java/com/teamwizardry/librarianlib/features/base/block/tile/TileMod.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/base/block/tile/TileMod.kt
@@ -60,21 +60,20 @@ abstract class TileMod : TileEntity() {
     private var modulesSetUp = false
 
     fun createModules() {
-        if (modulesSetUp) return
         if (world != null) {
-            modulesSetUp = true
-
             val state = world.getBlockState(pos)
             val block = state.block
             if (block is BlockModContainer && block.hasComparatorInputOverride == null)
                 block.hasComparatorInputOverride = hasComparatorOverride()
+        }
+        if (modulesSetUp) return
+        modulesSetUp = true
 
-            for ((name, field) in SavingFieldCache.getClassFields(FieldType.create(javaClass, null))) {
-                if (field.meta.hasFlag(SavingFieldFlag.MODULE)) {
-                    @Suppress("LeakingThis")
-                    val module = field.getter(this) as? ITileModule
-                    module?.let { initModule(name, it) }
-                }
+        for ((name, field) in SavingFieldCache.getClassFields(FieldType.create(javaClass, null))) {
+            if (field.meta.hasFlag(SavingFieldFlag.MODULE)) {
+                @Suppress("LeakingThis")
+                val module = field.getter(this) as? ITileModule
+                module?.let { initModule(name, it) }
             }
         }
     }
