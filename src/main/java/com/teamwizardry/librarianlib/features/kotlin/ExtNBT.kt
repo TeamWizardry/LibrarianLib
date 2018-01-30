35dfe8efa727cc83d7ee03fa92cffe5781dccb10
yrsegal <yrsegal@gmail.com>
2018-01-30 13:51:50 -0500

    serializers but simpler

--- src/main/java/com/teamwizardry/librarianlib/features/kotlin/ExtNBT.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/kotlin/ExtNBT.kt
@@ -37,21 +37,67 @@ inline fun <reified T : NBTBase> NBTBase.safeCast(): T = this.safeCast(T::class.
 fun <T : NBTBase> NBTBase.safeCast(clazz: Class<T>): T {
     return (when {
                 clazz.isAssignableFrom(this.javaClass) -> this
-                NBTPrimitive::class.java.isAssignableFrom(clazz) -> when (clazz) {
-                    NBTTagLong::class.java -> NBTTagLong(0)
-                    NBTTagInt::class.java -> NBTTagInt(0)
-                    NBTTagShort::class.java -> NBTTagShort(0)
-                    NBTTagDouble::class.java -> NBTTagDouble(0.0)
-                    NBTTagFloat::class.java -> NBTTagFloat(0f)
-                    else -> NBTTagByte(0)
-                }
-                clazz == NBTTagByteArray::class.java -> NBTTagByteArray(ByteArray(0))
-                clazz == NBTTagString::class.java -> NBTTagString("")
-                clazz == NBTTagList::class.java -> NBTTagList()
-                clazz == NBTTagCompound::class.java -> NBTTagCompound()
-                clazz == NBTTagIntArray::class.java -> NBTTagIntArray(IntArray(0))
-                else -> throw IllegalArgumentException("Unknown NBT type to cast to: $clazz")
+                else -> clazz.defaultNBT()
             }) as T
+}
+
+inline fun <reified T : NBTBase> defaultNBT(): T = T::class.java.defaultNBT()
+
+@Suppress("UNCHECKED_CAST")
+fun <T : NBTBase> Class<T>.defaultNBT(): T {
+    return (when {
+        NBTPrimitive::class.java.isAssignableFrom(this) -> when (this) {
+            NBTTagLong::class.java -> NBTTagLong(0)
+            NBTTagInt::class.java -> NBTTagInt(0)
+            NBTTagShort::class.java -> NBTTagShort(0)
+            NBTTagDouble::class.java -> NBTTagDouble(0.0)
+            NBTTagFloat::class.java -> NBTTagFloat(0f)
+            else -> NBTTagByte(0)
+        }
+        this == NBTTagByteArray::class.java -> NBTTagByteArray(ByteArray(0))
+        this == NBTTagString::class.java -> NBTTagString("")
+        this == NBTTagList::class.java -> NBTTagList()
+        this == NBTTagCompound::class.java -> NBTTagCompound()
+        this == NBTTagIntArray::class.java -> NBTTagIntArray(IntArray(0))
+        this == NBTTagLongArray::class.java -> NBTTagLongArray(LongArray(0))
+        else -> throw IllegalArgumentException("Unknown NBT type to produce: $this")
+    }) as T
+}
+
+fun Int.nbtClass(): Class<out NBTBase> {
+    return when (this) {
+        1 -> NBTTagByte::class.java
+        2 -> NBTTagShort::class.java
+        3 -> NBTTagInt::class.java
+        4 -> NBTTagLong::class.java
+        5 -> NBTTagFloat::class.java
+        6 -> NBTTagDouble::class.java
+        7 -> NBTTagByteArray::class.java
+        8 -> NBTTagString::class.java
+        9 -> NBTTagList::class.java
+        10 -> NBTTagCompound::class.java
+        11 -> NBTTagIntArray::class.java
+        12 -> NBTTagLongArray::class.java
+        else -> throw IllegalArgumentException("Unknown NBT type: $this")
+    }
+}
+
+fun Class<out NBTBase>.idForClazz(): Int {
+    return when (this) {
+        NBTTagByte::class.java -> 1
+        NBTTagShort::class.java -> 2
+        NBTTagInt::class.java -> 3
+        NBTTagLong::class.java -> 4
+        NBTTagFloat::class.java -> 5
+        NBTTagDouble::class.java -> 6
+        NBTTagByteArray::class.java -> 7
+        NBTTagString::class.java -> 8
+        NBTTagList::class.java -> 9
+        NBTTagCompound::class.java -> 10
+        NBTTagIntArray::class.java -> 11
+        NBTTagLongArray::class.java -> 12
+        else -> throw IllegalArgumentException("Unknown NBT type: $this")
+    }
 }
 
 class NBTWrapper(val contained: ItemStack) {
--- src/main/java/com/teamwizardry/librarianlib/features/saving/serializers/builtin/basics/Nbt.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/saving/serializers/builtin/basics/Nbt.kt
@@ -16,258 +16,74 @@ import net.minecraftforge.common.util.Constants
  */
 
 @SerializerRegister(NBTTagByte::class)
-object SerializeNBTTagByte : Serializer<NBTTagByte>(FieldType.create(NBTTagByte::class.java)) {
-    override fun getDefault()
-            = NBTTagByte(0)
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagByte?, syncing: Boolean): NBTTagByte
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagByte, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagByte?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_BYTE, buf) as NBTTagByte
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagByte, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagByte : NBTSerializer<NBTTagByte>(NBTTagByte::class.java)
 
 @SerializerRegister(NBTTagShort::class)
-object SerializeNBTTagShort : Serializer<NBTTagShort>(FieldType.create(NBTTagShort::class.java)) {
-    override fun getDefault()
-            = NBTTagShort(0)
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagShort?, syncing: Boolean): NBTTagShort
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagShort, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagShort?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_SHORT, buf) as NBTTagShort
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagShort, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagShort : NBTSerializer<NBTTagShort>(NBTTagShort::class.java)
 
 @SerializerRegister(NBTTagInt::class)
-object SerializeNBTTagInt : Serializer<NBTTagInt>(FieldType.create(NBTTagInt::class.java)) {
-    override fun getDefault()
-            = NBTTagInt(0)
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagInt?, syncing: Boolean): NBTTagInt
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagInt, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagInt?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_INT, buf) as NBTTagInt
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagInt, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagInt : NBTSerializer<NBTTagInt>(NBTTagInt::class.java)
 
 @SerializerRegister(NBTTagLong::class)
-object SerializeNBTTagLong : Serializer<NBTTagLong>(FieldType.create(NBTTagLong::class.java)) {
-    override fun getDefault()
-            = NBTTagLong(0L)
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagLong?, syncing: Boolean): NBTTagLong
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagLong, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagLong?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_LONG, buf) as NBTTagLong
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagLong, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagLong : NBTSerializer<NBTTagLong>(NBTTagLong::class.java)
 
 @SerializerRegister(NBTTagFloat::class)
-object SerializeNBTTagFloat : Serializer<NBTTagFloat>(FieldType.create(NBTTagFloat::class.java)) {
-    override fun getDefault()
-            = NBTTagFloat(0f)
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagFloat?, syncing: Boolean): NBTTagFloat
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagFloat, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagFloat?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_FLOAT, buf) as NBTTagFloat
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagFloat, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagFloat : NBTSerializer<NBTTagFloat>(NBTTagFloat::class.java)
 
 @SerializerRegister(NBTTagDouble::class)
-object SerializeNBTTagDouble : Serializer<NBTTagDouble>(FieldType.create(NBTTagDouble::class.java)) {
-    override fun getDefault()
-            = NBTTagDouble(0.0)
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagDouble?, syncing: Boolean): NBTTagDouble
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagDouble, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagDouble?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_DOUBLE, buf) as NBTTagDouble
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagDouble, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagDouble : NBTSerializer<NBTTagDouble>(NBTTagDouble::class.java)
 
 @SerializerRegister(NBTTagByteArray::class)
-object SerializeNBTTagByteArray : Serializer<NBTTagByteArray>(FieldType.create(NBTTagByteArray::class.java)) {
-    override fun getDefault()
-            = NBTTagByteArray(byteArrayOf())
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagByteArray?, syncing: Boolean): NBTTagByteArray
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagByteArray, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagByteArray?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_BYTE_ARRAY, buf) as NBTTagByteArray
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagByteArray, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagByteArray : NBTSerializer<NBTTagByteArray>(NBTTagByteArray::class.java)
 
 @SerializerRegister(NBTTagString::class)
-object SerializeNBTTagString : Serializer<NBTTagString>(FieldType.create(NBTTagString::class.java)) {
-    override fun getDefault()
-            = NBTTagString("")
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagString?, syncing: Boolean): NBTTagString
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagString, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagString?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_STRING, buf) as NBTTagString
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagString, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagString : NBTSerializer<NBTTagString>(NBTTagString::class.java)
 
 @SerializerRegister(NBTTagList::class)
-object SerializeNBTTagList : Serializer<NBTTagList>(FieldType.create(NBTTagList::class.java)) {
-    override fun getDefault()
-            = NBTTagList()
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagList?, syncing: Boolean): NBTTagList
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagList, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagList?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_LIST, buf) as NBTTagList
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagList, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagList : NBTSerializer<NBTTagList>(NBTTagList::class.java)
 
 @SerializerRegister(NBTTagCompound::class)
-object SerializeNBTTagCompound : Serializer<NBTTagCompound>(FieldType.create(NBTTagCompound::class.java)) {
-    override fun getDefault()
-            = NBTTagCompound()
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagCompound?, syncing: Boolean): NBTTagCompound
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagCompound, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagCompound?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_COMPOUND, buf) as NBTTagCompound
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagCompound, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagCompound : NBTSerializer<NBTTagCompound>(NBTTagCompound::class.java)
 
 @SerializerRegister(NBTTagIntArray::class)
-object SerializeNBTTagIntArray : Serializer<NBTTagIntArray>(FieldType.create(NBTTagIntArray::class.java)) {
-    override fun getDefault()
-            = NBTTagIntArray(intArrayOf())
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagIntArray?, syncing: Boolean): NBTTagIntArray
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagIntArray, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagIntArray?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_INT_ARRAY, buf) as NBTTagIntArray
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagIntArray, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagIntArray : NBTSerializer<NBTTagIntArray>(NBTTagIntArray::class.java)
 
 @SerializerRegister(NBTTagLongArray::class)
-object SerializeNBTTagLongArray : Serializer<NBTTagLongArray>(FieldType.create(NBTTagLongArray::class.java)) {
-    override fun getDefault()
-            = NBTTagLongArray(longArrayOf())
-
-    override fun readNBT(nbt: NBTBase, existing: NBTTagLongArray?, syncing: Boolean): NBTTagLongArray
-            = nbt.safeCast()
-
-    override fun writeNBT(value: NBTTagLongArray, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTTagLongArray?, syncing: Boolean)
-            = readTagFromBuffer(Constants.NBT.TAG_LONG_ARRAY, buf) as NBTTagLongArray
-
-    override fun writeBytes(buf: ByteBuf, value: NBTTagLongArray, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTTagLongArray : NBTSerializer<NBTTagLongArray>(NBTTagLongArray::class.java)
 
 @SerializerRegister(NBTBase::class)
-object SerializeNBTGeneric : Serializer<NBTBase>(FieldType.create(NBTBase::class.java)) {
-    override fun getDefault()
-            = NBTTagCompound()
-
-    override fun readNBT(nbt: NBTBase, existing: NBTBase?, syncing: Boolean)
-            = nbt
-
-    override fun writeNBT(value: NBTBase, syncing: Boolean): NBTBase
-            = value.copy()
-
-    override fun readBytes(buf: ByteBuf, existing: NBTBase?, syncing: Boolean)
-            = readTagFromBuffer(buf.readByte(), buf)
-
-    override fun writeBytes(buf: ByteBuf, value: NBTBase, syncing: Boolean)
-            = writeTagToBuffer(value, buf)
-}
+object SerializeNBTGeneric : GenericNBTSerializer<NBTBase>(NBTBase::class.java)
 
 @SerializerRegister(NBTPrimitive::class)
-object SerializeNBTPrimitive : Serializer<NBTPrimitive>(FieldType.create(NBTPrimitive::class.java)) {
-    override fun getDefault()
-            = NBTTagByte(0)
+object SerializeNBTPrimitive : GenericNBTSerializer<NBTPrimitive>(NBTPrimitive::class.java)
 
-    override fun readNBT(nbt: NBTBase, existing: NBTPrimitive?, syncing: Boolean): NBTPrimitive
-            = nbt.safeCast()
 
-    override fun writeNBT(value: NBTPrimitive, syncing: Boolean): NBTBase
+open class GenericNBTSerializer<T : NBTBase>(val clazz: Class<T>) : Serializer<T>(FieldType.create(clazz)) {
+
+    override fun getDefault(): T
+            = clazz.defaultNBT()
+
+    override fun readNBT(nbt: NBTBase, existing: T?, syncing: Boolean): T
+            = nbt.safeCast(clazz)
+
+    override fun writeNBT(value: T, syncing: Boolean): NBTBase
             = value.copy()
 
-    override fun readBytes(buf: ByteBuf, existing: NBTPrimitive?, syncing: Boolean)
-            = readTagFromBuffer(buf.readByte(), buf) as NBTPrimitive
+    @Suppress("UNCHECKED_CAST")
+    override fun readBytes(buf: ByteBuf, existing: T?, syncing: Boolean): T
+            = readTagFromBuffer(buf.readByte(), buf) as T
 
-    override fun writeBytes(buf: ByteBuf, value: NBTPrimitive, syncing: Boolean)
+    override fun writeBytes(buf: ByteBuf, value: T, syncing: Boolean)
             = writeTagToBuffer(value, buf)
 }
 
-fun readTagFromBuffer(id: Int, buf: ByteBuf) = readTagFromBuffer(id.toByte(), buf)
+open class NBTSerializer<T : NBTBase>(clazz: Class<T>) : GenericNBTSerializer<T>(clazz) {
+    val id = clazz.idForClazz().toByte()
+
+    @Suppress("UNCHECKED_CAST")
+    override fun readBytes(buf: ByteBuf, existing: T?, syncing: Boolean): T
+            = readTagFromBuffer(id, buf) as T
+}
 
 fun readTagFromBuffer(id: Byte, buf: ByteBuf): NBTBase {
     return when (id.toInt()) {
@@ -286,7 +102,7 @@ fun readTagFromBuffer(id: Byte, buf: ByteBuf): NBTBase {
         }
         Constants.NBT.TAG_COMPOUND -> buf.readTag()
         Constants.NBT.TAG_INT_ARRAY -> NBTTagIntArray(PacketBuffer(buf).readVarIntArray())
-        Constants.NBT.TAG_LONG_ARRAY -> NBTTagLongArray(buf.readLongArray())
+        Constants.NBT.TAG_LONG_ARRAY -> NBTTagLongArray(LongArray(buf.readVarInt()) { buf.readLong() })
         else -> NBTTagCompound()
     }
 }
@@ -314,12 +130,5 @@ fun writeTagToBuffer(tag: NBTBase, buf: ByteBuf) {
         is NBTTagCompound -> buf.writeTag(tag)
         is NBTTagIntArray -> PacketBuffer(buf).writeVarIntArray(tag.intArray)
         is NBTTagLongArray -> PacketBuffer(buf).writeLongArray(tag.longArray)
-    }
-}
-
-fun ByteBuf.readLongArray(): LongArray {
-    val i = this.readVarInt()
-    return LongArray(i) {
-        readLong()
     }
 }
