0918082d0896958f24aed25ee0dc12c4931efbed
yrsegal@gmail.com <yrsegal@gmail.com>
2018-08-08 18:30:16 -0400

    InterpFloatInOut lets you use... floats.
    This was silly and limiting for no good reason.

--- src/main/java/com/teamwizardry/librarianlib/features/math/interpolate/numeric/InterpFloatInOut.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/math/interpolate/numeric/InterpFloatInOut.kt
@@ -5,13 +5,18 @@ import kotlin.math.max
 import kotlin.math.min
 
 /**
- * Goes from 0 to 1 in [fadeIn] units of time, holds 1 for [normal] units, then goes to 0 in [fadeOut] units.
- *
- * [fadeIn], [normal], and [fadeOut] are proportions
+ * Linearly increases from 0 to 1 over the first [fadeInFraction] of an interval.
+ * Holds 1 until the last [fadeOutFraction] of that interval, and then decreases to 0.
  */
-class InterpFloatInOut(fadeIn: Int, normal: Int, fadeOut: Int) : InterpFunction<Float> {
-    val fadeInFraction = fadeIn.toFloat() / (fadeIn + normal + fadeOut)
-    val fadeOutFraction = fadeOut.toFloat() / (fadeIn + normal + fadeOut)
+class InterpFloatInOut(val fadeInFraction: Float, val fadeOutFraction: Float) : InterpFunction<Float> {
+
+    /**
+     * Goes from 0 to 1 in [fadeIn] units of time, holds 1 for [normal] units, then goes to 0 in [fadeOut] units.
+     *
+     * [fadeIn], [normal], and [fadeOut] are proportions
+     */
+    constructor(fadeIn: Int, normal: Int, fadeOut: Int) : this(fadeIn.toFloat() / (fadeIn + normal + fadeOut),
+            fadeOut.toFloat() / (fadeIn + normal + fadeOut))
 
     @Deprecated(message = "Confusingly named", replaceWith = ReplaceWith("fadeInFraction"))
     val fadeInEnd = fadeInFraction
@@ -19,8 +24,8 @@ class InterpFloatInOut(fadeIn: Int, normal: Int, fadeOut: Int) : InterpFunction<
     val fadeOutStart = fadeOutFraction
 
     override fun get(i: Float): Float {
-        val fadeInResult = i/fadeInFraction
-        val fadeOutResult = (1-i)/fadeOutFraction
+        val fadeInResult = i / fadeInFraction
+        val fadeOutResult = (1 - i) / fadeOutFraction
         return max(0f, min(1f, min(fadeInResult, fadeOutResult)))
     }
 
