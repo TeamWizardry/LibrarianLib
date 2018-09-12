04e648949ddc48ae897597e66f4b00559bc7f839
yrsegal@gmail.com <yrsegal@gmail.com>
2018-09-11 23:18:39 -0400

    Add a vector rotation utility
    This was something I had been planning for a long time, but never got around to doing. The main purpose is to be able to rotate AABBs using the BoundingBoxTransformer.

--- src/main/java/com/teamwizardry/librarianlib/features/kotlin/ExtVecMath.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/kotlin/ExtVecMath.kt
@@ -4,45 +4,101 @@
 package com.teamwizardry.librarianlib.features.kotlin
 
 import com.teamwizardry.librarianlib.features.math.Vec2d
+import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
 import net.minecraft.util.math.Vec3i
+import javax.vecmath.Matrix3d
+import javax.vecmath.Matrix3f
+import javax.vecmath.Matrix4f
+import kotlin.math.acos
 
 // Vec3d ===============================================================================================================
 
-operator fun Vec3d.times(other: Vec3d): Vec3d = Vec3d(this.x * other.x, this.y * other.y, this.z * other.z)
-operator fun Vec3d.times(other: Double): Vec3d = this.scale(other)
+operator fun Vec3d.times(other: Vec3d): Vec3d = Vec3d(x * other.x, y * other.y, z * other.z)
+operator fun Vec3d.times(other: Double): Vec3d = scale(other)
 operator fun Vec3d.times(other: Float): Vec3d = this * other.toDouble()
 operator fun Vec3d.times(other: Int): Vec3d = this * other.toDouble()
 
-operator fun Vec3d.div(other: Vec3d) = Vec3d(this.x / other.x, this.y / other.y, this.z / other.z)
+operator fun Vec3d.div(other: Vec3d) = Vec3d(x / other.x, y / other.y, z / other.z)
 operator fun Vec3d.div(other: Double): Vec3d = this * (1 / other)
 operator fun Vec3d.div(other: Float): Vec3d = this / other.toDouble()
 operator fun Vec3d.div(other: Int): Vec3d = this / other.toDouble()
 
-operator fun Vec3d.plus(other: Vec3d): Vec3d = this.add(other)
-operator fun Vec3d.minus(other: Vec3d): Vec3d = this.subtract(other)
+operator fun Vec3d.plus(other: Vec3d): Vec3d = add(other)
+operator fun Vec3d.minus(other: Vec3d): Vec3d = subtract(other)
 operator fun Vec3d.unaryMinus(): Vec3d = this * -1.0
 
-infix fun Vec3d.dot(other: Vec3d) = this.dotProduct(other)
+infix fun Vec3d.dot(other: Vec3d) = dotProduct(other)
 
-infix fun Vec3d.cross(other: Vec3d): Vec3d = this.crossProduct(other)
+infix fun Vec3d.cross(other: Vec3d): Vec3d = crossProduct(other)
 
-fun Vec3d.withX(other: Double) = Vec3d(other, this.y, this.z)
-fun Vec3d.withY(other: Double) = Vec3d(this.x, other, this.z)
-fun Vec3d.withZ(other: Double) = Vec3d(this.x, this.y, other)
+infix fun Vec3d.angle(other: Vec3d) = acos((this dot other) / (length() * other.length()))
 
-fun Vec3d.withX(other: Float) = this.withX(other.toDouble())
-fun Vec3d.withY(other: Float) = this.withY(other.toDouble())
-fun Vec3d.withZ(other: Float) = this.withZ(other.toDouble())
+fun Vec3d.withX(other: Double) = Vec3d(other, y, z)
+fun Vec3d.withY(other: Double) = Vec3d(x, other, z)
+fun Vec3d.withZ(other: Double) = Vec3d(x, y, other)
 
-fun Vec3d.withX(other: Int) = this.withX(other.toDouble())
-fun Vec3d.withY(other: Int) = this.withY(other.toDouble())
-fun Vec3d.withZ(other: Int) = this.withZ(other.toDouble())
+fun Vec3d.withX(other: Float) = withX(other.toDouble())
+fun Vec3d.withY(other: Float) = withY(other.toDouble())
+fun Vec3d.withZ(other: Float) = withZ(other.toDouble())
+
+fun Vec3d.withX(other: Int) = withX(other.toDouble())
+fun Vec3d.withY(other: Int) = withY(other.toDouble())
+fun Vec3d.withZ(other: Int) = withZ(other.toDouble())
+
+val Vec3d.crossMatrix get() = Matrix3d(
+        0.0, -z, y,
+        z, 0.0, -x,
+        -y, x, 0.0)
+val Vec3d.tensorMatrix get() = Matrix3d(
+        x * x, x * y, x * z,
+        y * x, y * y, y * z,
+        z * x, z * y, z * z)
 
 operator fun Vec3d.component1() = x
 operator fun Vec3d.component2() = y
 operator fun Vec3d.component3() = z
+
+// Vec3i ===============================================================================================================
+
+
+fun Vec3i.lengthSquared() = x * x + y * y + z * z
+
+fun Vec3i.length() = Math.sqrt(lengthSquared().toDouble())
+
+operator fun Vec3i.times(other: Vec3i): Vec3i = Vec3i(x * other.x, y * other.y, z * other.z)
+operator fun Vec3i.times(other: Double): Vec3i = Vec3i(x * other, y * other, z * other)
+operator fun Vec3i.times(other: Float): Vec3i = this * other.toDouble()
+operator fun Vec3i.times(other: Int): Vec3i = this * other.toDouble()
+
+operator fun Vec3i.div(other: Vec3i) = Vec3i(x / other.x, y / other.y, z / other.z)
+operator fun Vec3i.div(other: Double): Vec3i = this * (1 / other)
+operator fun Vec3i.div(other: Float): Vec3i = this / other.toDouble()
+operator fun Vec3i.div(other: Int): Vec3i = this / other.toDouble()
+
+operator fun Vec3i.plus(other: Vec3i): Vec3i = Vec3i(x + other.x, y + other.y, z + other.z)
+operator fun Vec3i.minus(other: Vec3i): Vec3i = Vec3i(x - other.x, y - other.y, z - other.z)
+operator fun Vec3i.unaryMinus(): Vec3i = this * -1
+
+infix fun Vec3i.dot(other: Vec3i) = x * other.x + y * other.y + z * other.z
+
+infix fun Vec3i.cross(other: Vec3i): Vec3i = crossProduct(other)
+
+infix fun Vec3i.angle(other: Vec3i) = acos((this dot other) / (length() * other.length()))
+
+fun Vec3i.withX(other: Int) = Vec3i(other, y, z)
+fun Vec3i.withY(other: Int) = Vec3i(x, other, z)
+fun Vec3i.withZ(other: Int) = Vec3i(x, y, other)
+
+val Vec3i.crossMatrix get() = Matrix3d(
+        0.0, -z * 1.0, y * 1.0,
+        z * 1.0, 0.0, -x * 1.0,
+        -y * 1.0, x * 1.0, 0.0)
+val Vec3i.tensorMatrix get() = Matrix3d(
+        x * x * 1.0, x * y * 1.0, x * z * 1.0,
+        y * x * 1.0, y * y * 1.0, y * z * 1.0,
+        z * x * 1.0, z * y * 1.0, z * z * 1.0)
 
 operator fun Vec3i.component1() = x
 operator fun Vec3i.component2() = y
@@ -63,45 +119,55 @@ fun randomNormal(): Vec3d {
     return Vec3d(x, y, z)
 }
 
+
 // Vec2d ===============================================================================================================
 
-operator fun Vec2d.times(other: Vec2d) = this.mul(other)
-operator fun Vec2d.times(other: Double) = this.mul(other)
+operator fun Vec2d.times(other: Vec2d) = mul(other)
+operator fun Vec2d.times(other: Double) = mul(other)
 operator fun Vec2d.times(other: Float) = this * other.toDouble()
 operator fun Vec2d.times(other: Int) = this * other.toDouble()
 
-operator fun Vec2d.div(other: Vec2d) = this.divide(other)
-operator fun Vec2d.div(other: Double) = this.divide(other)
+operator fun Vec2d.div(other: Vec2d) = divide(other)
+operator fun Vec2d.div(other: Double) = divide(other)
 operator fun Vec2d.div(other: Float) = this / other.toDouble()
 operator fun Vec2d.div(other: Int) = this / other.toDouble()
 
-operator fun Vec2d.plus(other: Vec2d) = this.add(other)
-operator fun Vec2d.minus(other: Vec2d) = this.sub(other)
+operator fun Vec2d.plus(other: Vec2d) = add(other)
+operator fun Vec2d.minus(other: Vec2d) = sub(other)
 operator fun Vec2d.unaryMinus() = this * -1
 
-fun Vec2d.withX(other: Double) = Vec2d(other, this.y)
-fun Vec2d.withY(other: Double) = Vec2d(this.x, other)
+fun Vec2d.withX(other: Double) = Vec2d(other, y)
+fun Vec2d.withY(other: Double) = Vec2d(x, other)
 
-fun Vec2d.withX(other: Float) = this.withX(other.toDouble())
-fun Vec2d.withY(other: Float) = this.withY(other.toDouble())
+fun Vec2d.withX(other: Float) = withX(other.toDouble())
+fun Vec2d.withY(other: Float) = withY(other.toDouble())
 
-fun Vec2d.withX(other: Int) = this.withX(other.toDouble())
-fun Vec2d.withY(other: Int) = this.withY(other.toDouble())
+fun Vec2d.withX(other: Int) = withX(other.toDouble())
+fun Vec2d.withY(other: Int) = withY(other.toDouble())
 
-operator fun Vec2d.component1() = x
-operator fun Vec2d.component2() = y
 
 // BlockPos ============================================================================================================
 
-operator fun BlockPos.times(other: BlockPos) = BlockPos(this.x * other.x, this.y * other.y, this.z * other.z)
-operator fun BlockPos.times(other: Vec3d) = BlockPos((this.x * other.x).toInt(), (this.y * other.y).toInt(), (this.z * other.z).toInt())
-operator fun BlockPos.times(other: Number) = BlockPos((this.x * other.toDouble()).toInt(), (this.y * other.toDouble()).toInt(), (this.z * other.toDouble()).toInt())
+operator fun BlockPos.times(other: BlockPos) = BlockPos(x * other.x, y * other.y, z * other.z)
+operator fun BlockPos.times(other: Vec3d) = BlockPos((x * other.x).toInt(), (y * other.y).toInt(), (z * other.z).toInt())
+operator fun BlockPos.times(other: Number) = BlockPos((x * other.toDouble()).toInt(), (y * other.toDouble()).toInt(), (z * other.toDouble()).toInt())
 
-operator fun BlockPos.div(other: BlockPos) = BlockPos(this.x / other.x, this.y / other.y, this.z / other.z)
-operator fun BlockPos.div(other: Vec3d) = BlockPos((this.x / other.x).toInt(), (this.y / other.y).toInt(), (this.z / other.z).toInt())
-operator fun BlockPos.div(other: Number) = BlockPos((this.x / other.toDouble()).toInt(), (this.y / other.toDouble()).toInt(), (this.z / other.toDouble()).toInt())
+operator fun BlockPos.div(other: BlockPos) = BlockPos(x / other.x, y / other.y, z / other.z)
+operator fun BlockPos.div(other: Vec3d) = BlockPos((x / other.x).toInt(), (y / other.y).toInt(), (z / other.z).toInt())
+operator fun BlockPos.div(other: Number) = BlockPos((x / other.toDouble()).toInt(), (y / other.toDouble()).toInt(), (z / other.toDouble()).toInt())
 
-operator fun BlockPos.plus(other: BlockPos): BlockPos = this.add(other)
-operator fun BlockPos.minus(other: BlockPos): BlockPos = this.subtract(other)
+operator fun BlockPos.plus(other: BlockPos): BlockPos = add(other)
+operator fun BlockPos.minus(other: BlockPos): BlockPos = subtract(other)
 
-operator fun BlockPos.unaryMinus() = this * -1
+
+// Matrices ============================================================================================================
+
+operator fun Matrix3f.times(scalar: Float) = apply { mul(scalar) }
+operator fun Matrix3f.times(other: Matrix3f) = apply { mul(other) }
+operator fun Matrix3f.plus(other: Matrix3f) = apply { add(other) }
+operator fun Matrix3f.minus(other: Matrix3f) = apply { sub(other) }
+
+operator fun Matrix4f.times(scalar: Float) = apply { mul(scalar) }
+operator fun Matrix4f.times(other: Matrix4f) = apply { mul(other) }
+operator fun Matrix4f.plus(other: Matrix4f) = apply { add(other) }
+operator fun Matrix4f.minus(other: Matrix4f) = apply { sub(other) }
---
+++ src/main/java/com/teamwizardry/librarianlib/features/math/Rotate3.kt
@@ -0,0 +1,218 @@
+@file:JvmName("Rotations3D")
+package com.teamwizardry.librarianlib.features.math
+
+import com.teamwizardry.librarianlib.features.kotlin.angle
+import com.teamwizardry.librarianlib.features.kotlin.cross
+import com.teamwizardry.librarianlib.features.kotlin.times
+import com.teamwizardry.librarianlib.features.kotlin.unaryMinus
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.math.AxisAlignedBB
+import net.minecraft.util.math.Vec3d
+import net.minecraft.util.math.Vec3i
+import javax.vecmath.AxisAngle4d
+import javax.vecmath.Matrix3f
+import javax.vecmath.Matrix4f
+import javax.vecmath.Vector3f
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * @author WireSegal
+ * Created at 4:43 PM on 9/11/18.
+ */
+
+typealias LinearTransformation = Matrix3f
+typealias AffineTransformation = Matrix4f
+
+private val I = Vec3d(1.0, 0.0, 0.0)
+private val J = Vec3d(0.0, 1.0, 0.0)
+private val K = Vec3d(0.0, 0.0, 1.0)
+
+private val BLOCK_CENTER = Vec3d(0.5, 0.5, 0.5)
+
+private val IDENTITY = LinearTransformation().apply { setIdentity() }
+
+private val Vec3d.javax get() = Vector3f(x.toFloat(), y.toFloat(), z.toFloat())
+
+private fun identity() = LinearTransformation().apply { setIdentity() }
+
+/**
+ * Creates a [LinearTransformation] that rotates inputs:
+ * * [xAngle] around the X axis
+ * * [yAngle] around the Y axis
+ * * [zAngle] around the Z axis
+ */
+fun rotationMatrix(xAngle: Double, yAngle: Double, zAngle: Double) =
+        rotationMatrix(xAngle, I) * rotationMatrix(yAngle, J) * rotationMatrix(zAngle, K)
+
+/**
+ * Creates a [LinearTransformation] that rotates inputs [angle] around the axis given by ([axisX], [axisY], [axisZ]).
+ */
+fun rotationMatrix(angle: Double, axisX: Double, axisY: Double, axisZ: Double) =
+        LinearTransformation().apply { set(AxisAngle4d(axisX, axisY, axisZ, angle)) }
+
+/**
+ * Creates a [LinearTransformation] that rotates inputs [angle] around the axis given by [axis].
+ */
+fun rotationMatrix(angle: Double, axis: Vec3i) = rotationMatrix(angle, axis.x * 1.0, axis.y * 1.0, axis.z * 1.0)
+
+/**
+ * Creates a [LinearTransformation] that rotates inputs [angle] around the axis given by [axis].
+ */
+fun rotationMatrix(angle: Double, axis: Vec3d) = rotationMatrix(angle, axis.x, axis.y, axis.z)
+
+/**
+ * Creates a [LinearTransformation] that rotates inputs from [from] to [to]. (i.e. north to east)
+ */
+fun rotationMatrix(from: Vec3d, to: Vec3d): LinearTransformation {
+    if (from == -to) return identity().apply { negate() }
+    return rotationMatrix(from angle to, from cross to)
+}
+
+/**
+ * Creates a [LinearTransformation] that rotates inputs from [from] to [to]. (i.e. north to east)
+ */
+fun rotationMatrix(from: Vec3i, to: Vec3i): LinearTransformation {
+    if (from == -to) return identity().apply { negate() }
+    return rotationMatrix(from angle to, from cross to)
+}
+
+/**
+ * Creates a [LinearTransformation] that rotates inputs from [from] to [to]. (i.e. north to east)
+ */
+fun rotationMatrix(from: EnumFacing, to: EnumFacing) = rotationMatrix(from.directionVec, to.directionVec)
+
+/**
+ * Creates an affine transformation matrix that translates inputs by the vector [translate].
+ */
+fun translation(translate: Vec3d) = AffineTransformation(IDENTITY, translate.javax, 1f)
+
+/**
+ * Converts a [LinearTransformation] to an [AffineTransformation] with no translation.
+ */
+fun LinearTransformation.affine() = withTranslation(Vec3d.ZERO)
+
+/**
+ * Converts a [LinearTransformation] to an [AffineTransformation] translated to the point [point].
+ */
+fun LinearTransformation.withTranslation(point: Vec3d) = AffineTransformation(this, point.javax, 1f)
+
+/**
+ * Converts a [LinearTransformation] to an [AffineTransformation] centered around the block center (0.5, 0.5, 0.5).
+ */
+fun LinearTransformation.aroundCenter() = affine().aroundCenter()
+
+/**
+ * Converts a [LinearTransformation] to an [AffineTransformation] centered around the point [point].
+ */
+fun LinearTransformation.aroundPoint(point: Vec3d) = affine().aroundPoint(point)
+
+/**
+ * Centers an [AffineTransformation] around the block center (0.5, 0.5, 0.5).
+ */
+fun AffineTransformation.aroundCenter() = aroundPoint(BLOCK_CENTER)
+
+/**
+ * Centers an [AffineTransformation] around the point [point].
+ */
+fun AffineTransformation.aroundPoint(point: Vec3d) = translation(point) * this * translation(-point)
+
+/**
+ * Applies a [LinearTransformation] to the vector ([x], [y], [z]).
+ */
+fun LinearTransformation.rotate(x: Double, y: Double, z: Double): Vec3d {
+    val newX = m00 * x + m01 * y + m02 * z
+    val newY = m10 * x + m11 * y + m12 * z
+    val newZ = m20 * x + m21 * y + m22 * z
+    return Vec3d(newX, newY, newZ)
+}
+
+/**
+ * Applies a [LinearTransformation] to the vector [point].
+ */
+fun LinearTransformation.rotate(point: Vec3d) = rotate(point.x, point.y, point.z)
+
+/**
+ * Applies a [LinearTransformation] to the vector [point].
+ */
+fun LinearTransformation.rotate(point: Vec3i) = rotate(point.x * 1.0, point.y * 1.0, point.z * 1.0)
+
+/**
+ * Applies a [LinearTransformation] to the axis-aligned bounding box [boundingBox].
+ */
+fun LinearTransformation.rotate(boundingBox: AxisAlignedBB): AxisAlignedBB {
+    val bbMinX = boundingBox.minX - 0.5
+    val bbMinY = boundingBox.minY - 0.5
+    val bbMinZ = boundingBox.minZ - 0.5
+    val bbMaxX = boundingBox.maxX - 0.5
+    val bbMaxY = boundingBox.maxY - 0.5
+    val bbMaxZ = boundingBox.maxZ - 0.5
+
+    val p1 = rotate(bbMinX, bbMinY, bbMinZ)
+    val p2 = rotate(bbMinX, bbMinY, bbMaxZ)
+    val p3 = rotate(bbMinX, bbMaxY, bbMinZ)
+    val p4 = rotate(bbMinX, bbMaxY, bbMaxZ)
+    val p5 = rotate(bbMaxX, bbMinY, bbMinZ)
+    val p6 = rotate(bbMaxX, bbMinY, bbMaxZ)
+    val p7 = rotate(bbMaxX, bbMaxY, bbMinZ)
+    val p8 = rotate(bbMaxX, bbMaxY, bbMaxZ)
+
+    val minX = min(p1.x, min(p2.x, min(p3.x, min(p4.x, min(p5.x, min(p6.x, min(p7.x, p8.x)))))))
+    val minY = min(p1.y, min(p2.y, min(p3.y, min(p4.y, min(p5.y, min(p6.y, min(p7.y, p8.y)))))))
+    val minZ = min(p1.z, min(p2.z, min(p3.z, min(p4.z, min(p5.z, min(p6.z, min(p7.z, p8.z)))))))
+    val maxX = max(p1.x, max(p2.x, max(p3.x, max(p4.x, max(p5.x, max(p6.x, max(p7.x, p8.x)))))))
+    val maxY = max(p1.y, max(p2.y, max(p3.y, max(p4.y, max(p5.y, max(p6.y, max(p7.y, p8.y)))))))
+    val maxZ = max(p1.z, max(p2.z, max(p3.z, max(p4.z, max(p5.z, max(p6.z, max(p7.z, p8.z)))))))
+
+    return AxisAlignedBB(minX, minY, minZ, maxX, maxY, maxZ)
+}
+
+/**
+ * Applies an [AffineTransformation] to the vector ([x], [y], [z]).
+ */
+fun AffineTransformation.rotate(x: Double, y: Double, z: Double): Vec3d {
+    val newX = m00 * x + m01 * y + m02 * z + m03
+    val newY = m10 * x + m11 * y + m12 * z + m13
+    val newZ = m20 * x + m21 * y + m22 * z + m23
+    return Vec3d(newX, newY, newZ)
+}
+
+/**
+ * Applies an [AffineTransformation] to the vector [point].
+ */
+fun AffineTransformation.rotate(point: Vec3d) = rotate(point.x, point.y, point.z)
+
+/**
+ * Applies an [AffineTransformation] to the vector [point].
+ */
+fun AffineTransformation.rotate(point: Vec3i) = rotate(point.x * 1.0, point.y * 1.0, point.z * 1.0)
+
+/**
+ * Applies an [AffineTransformation] to the axis-aligned bounding box [boundingBox].
+ */
+fun AffineTransformation.rotate(boundingBox: AxisAlignedBB): AxisAlignedBB {
+    val bbMinX = boundingBox.minX - 0.5
+    val bbMinY = boundingBox.minY - 0.5
+    val bbMinZ = boundingBox.minZ - 0.5
+    val bbMaxX = boundingBox.maxX - 0.5
+    val bbMaxY = boundingBox.maxY - 0.5
+    val bbMaxZ = boundingBox.maxZ - 0.5
+
+    val p1 = rotate(bbMinX, bbMinY, bbMinZ)
+    val p2 = rotate(bbMinX, bbMinY, bbMaxZ)
+    val p3 = rotate(bbMinX, bbMaxY, bbMinZ)
+    val p4 = rotate(bbMinX, bbMaxY, bbMaxZ)
+    val p5 = rotate(bbMaxX, bbMinY, bbMinZ)
+    val p6 = rotate(bbMaxX, bbMinY, bbMaxZ)
+    val p7 = rotate(bbMaxX, bbMaxY, bbMinZ)
+    val p8 = rotate(bbMaxX, bbMaxY, bbMaxZ)
+
+    val minX = min(p1.x, min(p2.x, min(p3.x, min(p4.x, min(p5.x, min(p6.x, min(p7.x, p8.x)))))))
+    val minY = min(p1.y, min(p2.y, min(p3.y, min(p4.y, min(p5.y, min(p6.y, min(p7.y, p8.y)))))))
+    val minZ = min(p1.z, min(p2.z, min(p3.z, min(p4.z, min(p5.z, min(p6.z, min(p7.z, p8.z)))))))
+    val maxX = max(p1.x, max(p2.x, max(p3.x, max(p4.x, max(p5.x, max(p6.x, max(p7.x, p8.x)))))))
+    val maxY = max(p1.y, max(p2.y, max(p3.y, max(p4.y, max(p5.y, max(p6.y, max(p7.y, p8.y)))))))
+    val maxZ = max(p1.z, max(p2.z, max(p3.z, max(p4.z, max(p5.z, max(p6.z, max(p7.z, p8.z)))))))
+
+    return AxisAlignedBB(minX, minY, minZ, maxX, maxY, maxZ)
+}
