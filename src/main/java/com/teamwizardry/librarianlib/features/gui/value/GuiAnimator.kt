5424065acc62e00b6a3d53a87c71e606d41fa054
Pierce Corcoran <pierce@plasticcow.com>
2018-09-13 15:38:18 -0700

    Write GuiAnimator docs

--- src/main/java/com/teamwizardry/librarianlib/features/gui/value/GuiAnimator.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/gui/value/GuiAnimator.kt
@@ -61,10 +61,60 @@ class GuiAnimator {
                 return threadLocal.get() ?: GuiAnimator().also { threadLocal.set(it) }
             }
 
-        fun animate(duration: Float, easing: Easing = Easing.linear, block: () -> Unit): Animation<*> {
+        /**
+         * This method will create an animation that interpolates between the pre- and post-block values of any
+         * [IMValues][IMValue] or [RMValues][RMValue] that are modified within the passed block.
+         *
+         * If you aren't sure whether you can animate a particular property with this, jump to the LibrarianLib
+         * source code and check if the property delegates to an [IMValue] or [RMValue].
+         * (because LibrarianLib is kotlin `SomeType getSomeProperty` in java will map to
+         * `var/val someProperty: SomeType` in kotlin. Delegation is defined using the `by` keyword, so if
+         * `by RMValue...` or `by someIMValueProperty` appear after the property declaration that means you can
+         * animate it using this method.
+         *
+         * The internal process by which animations are created is as follows:
+         * - The first time any given [IMValue] or [RMValue] is modified inside of [block] it registers
+         * itself using `GuiAnimator.`[current][current]`.`[add][add] and its pre-change value is saved as the
+         * starting value of the animation.
+         * - After the block finishes the value of each [GuiAnimatable] object that was registered is read as the
+         * ending value of the animation.
+         * - If the start and end are equal or either is null then no animations will be added for that value.
+         *
+         * @param duration The duration of the returned animation
+         * @param easing The easing value of the returned animation
+         * @param block The block that will be run and inside of which any changes will occur
+         * @return The generated animation
+         */
+        @JvmOverloads
+        fun animate(duration: Float, easing: Easing = Easing.linear, block: Runnable): Animation<*> {
             return current.animate(duration, easing, block)
         }
-        fun animate(duration: Float, easing: Easing = Easing.linear, block: Runnable): Animation<*> {
+
+        /**
+         * This method will create an animation that interpolates between the pre- and post-block values of any
+         * [IMValues][IMValue] or [RMValues][RMValue] that are modified within the passed block.
+         *
+         * If you aren't sure whether you can animate a particular property with this, jump to the LibrarianLib
+         * source code and check if the property delegates to an [IMValue] or [RMValue].
+         * (because LibrarianLib is kotlin `SomeType getSomeProperty` in java will map to
+         * `var/val someProperty: SomeType` in kotlin. Delegation is defined using the `by` keyword, so if
+         * `by RMValue...` or `by someIMValueProperty` appear after the property declaration that means you can
+         * animate it using this method.
+         *
+         * The internal process by which animations are created is as follows:
+         * - The first time any given [IMValue] or [RMValue] is modified inside of [block] it registers
+         * itself using `GuiAnimator.`[current][current]`.`[add][add] and its pre-change value is saved as the
+         * starting value of the animation.
+         * - After the block finishes the value of each [GuiAnimatable] object that was registered is read as the
+         * ending value of the animation.
+         * - If the start and end are equal or either is null then no animations will be added for that value.
+         *
+         * @param duration The duration of the returned animation
+         * @param easing The easing value of the returned animation
+         * @param block The block that will be run and inside of which any changes will occur
+         * @return The generated animation
+         */
+        fun animate(duration: Float, easing: Easing = Easing.linear, block: () -> Unit): Animation<*> {
             return current.animate(duration, easing, block)
         }
     }
@@ -73,13 +123,28 @@ class GuiAnimator {
     private var isAnimating = false
 
     /**
-     * Runs the passed block and creates an animation for each [IMValue] and [RMValue] that changes inside it.
+     * This method will create an animation that interpolates between the pre- and post-block values of any
+     * [IMValues][IMValue] or [RMValues][RMValue] that are modified within the passed block.
      *
-     * The first time a given [IMValue] or [RMValue] is modified inside of [block] the value is saved. This will be
-     * used as the start of the animation.
-     * After the block finishes each I/RMValue that is going to be included in the animation has its value read.
-     * These values will be used as the ends of each animation.
-     * If the start and end values are equal or either is null no animations will be added for that I/RMValue.
+     * If you aren't sure whether you can animate a particular property with this, jump to the LibrarianLib
+     * source code and check if the property delegates to an [IMValue] or [RMValue].
+     * (because LibrarianLib is kotlin `SomeType getSomeProperty` in java will map to
+     * `var/val someProperty: SomeType` in kotlin. Delegation is defined using the `by` keyword, so if
+     * `by RMValue...` or `by someIMValueProperty` appear after the property declaration that means you can
+     * animate it using this method.
+     *
+     * The internal process by which animations are created is as follows:
+     * - The first time any given [IMValue] or [RMValue] is modified inside of [block] it registers
+     * itself using `GuiAnimator.`[current][current]`.`[add][add] and its pre-change value is saved as the
+     * starting value of the animation.
+     * - After the block finishes the value of each [GuiAnimatable] object that was registered is read as the
+     * ending value of the animation.
+     * - If the start and end are equal or either is null then no animations will be added for that value.
+     *
+     * @param duration The duration of the returned animation
+     * @param easing The easing value of the returned animation
+     * @param block The block that will be run and inside of which any changes will occur
+     * @return The generated animation
      */
     @JvmOverloads
     fun animate(duration: Float, easing: Easing = Easing.linear, block: Runnable): Animation<*> {
@@ -107,7 +172,28 @@ class GuiAnimator {
     }
 
     /**
-     * @see animate
+     * This method will create an animation that interpolates between the pre- and post-block values of any
+     * [IMValues][IMValue] or [RMValues][RMValue] that are modified within the passed block.
+     *
+     * If you aren't sure whether you can animate a particular property with this, jump to the LibrarianLib
+     * source code and check if the property delegates to an [IMValue] or [RMValue].
+     * (because LibrarianLib is kotlin `SomeType getSomeProperty` in java will map to
+     * `var/val someProperty: SomeType` in kotlin. Delegation is defined using the `by` keyword, so if
+     * `by RMValue...` or `by someIMValueProperty` appear after the property declaration that means you can
+     * animate it using this method.
+     *
+     * The internal process by which animations are created is as follows:
+     * - The first time any given [IMValue] or [RMValue] is modified inside of [block] it registers
+     * itself using `GuiAnimator.`[current][current]`.`[add][add] and its pre-change value is saved as the
+     * starting value of the animation.
+     * - After the block finishes the value of each [GuiAnimatable] object that was registered is read as the
+     * ending value of the animation.
+     * - If the start and end are equal or either is null then no animations will be added for that value.
+     *
+     * @param duration The duration of the returned animation
+     * @param easing The easing value of the returned animation
+     * @param block The block that will be run and inside of which any changes will occur
+     * @return The generated animation
      */
     fun animate(duration: Float, easing: Easing = Easing.linear, block: () -> Unit): Animation<*> {
         return this.animate(duration, easing, Runnable(block))
