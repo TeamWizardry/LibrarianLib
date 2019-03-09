ebcf265d7586a5a0bf4dc7d5e6b8809380eba072
Pierce Corcoran <pierce@plasticcow.com>
2019-03-09 14:00:30 -0800

    Fix CoordinateSpace2D matrix to itself

--- src/main/java/com/teamwizardry/librarianlib/features/math/coordinatespaces/CoordinateSpace2D.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/math/coordinatespaces/CoordinateSpace2D.kt
@@ -19,6 +19,7 @@ interface CoordinateSpace2D {
      */
     @JvmDefault
     fun conversionMatrixTo(other: CoordinateSpace2D): Matrix3 {
+        if(other === this) return Matrix3.identity
         if(other === this.parentSpace) return this.matrix.copy()
         if(other.parentSpace === this) return other.inverseMatrix.copy()
 
