ce2df2ff6a51c3a21640ffc3b8c444b1f3d4571e
Pierce Corcoran <pierce@plasticcow.com>
2019-06-01 20:38:15 -0700

    Remove JavaFX clipboard dependency

--- src/main/java/com/teamwizardry/librarianlib/features/text/Clipboard.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/text/Clipboard.kt
@@ -1,34 +1,15 @@
 package com.teamwizardry.librarianlib.features.text
 
 import games.thecodewarrior.bitfont.editor.utils.Clipboard
-import java.awt.datatransfer.DataFlavor
-import java.awt.Toolkit.getDefaultToolkit
-import javafx.scene.input.Clipboard.getSystemClipboard
-import java.awt.Toolkit
-import java.awt.datatransfer.Transferable
-import java.awt.datatransfer.ClipboardOwner
-import java.awt.datatransfer.StringSelection
+import net.minecraft.client.gui.GuiScreen
 
 object MCClipboard: Clipboard {
     override var contents: String?
         get() {
-            try {
-                val transferable = Toolkit.getDefaultToolkit().systemClipboard.getContents(null)
-
-                if (transferable != null && transferable.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-                    return transferable.getTransferData(DataFlavor.stringFlavor) as String
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-            return ""
+            return GuiScreen.getClipboardString()
         }
         set(value) {
-            try {
-                val stringselection = StringSelection(value)
-                Toolkit.getDefaultToolkit().systemClipboard.setContents(stringselection, null)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+            if(value == null) return
+            GuiScreen.setClipboardString(value)
         }
 }
