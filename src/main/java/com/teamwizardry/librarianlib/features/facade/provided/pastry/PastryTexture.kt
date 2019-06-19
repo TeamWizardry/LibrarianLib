04fe6a40eebd78b465da2a3e31a86f9d2f98608c
Pierce Corcoran <pierce@plasticcow.com>
2019-06-19 15:18:02 -0700

    Much more functional scroll pane

--- src/main/java/com/teamwizardry/librarianlib/features/facade/provided/pastry/PastryTexture.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/facade/provided/pastry/PastryTexture.kt
@@ -4,6 +4,7 @@ import com.teamwizardry.librarianlib.features.kotlin.toRl
 import com.teamwizardry.librarianlib.features.sprite.Sprite
 import com.teamwizardry.librarianlib.features.sprite.Texture
 import net.minecraft.util.ResourceLocation
+import java.awt.Color
 
 object PastryTexture {
 
@@ -19,70 +20,72 @@ object PastryTexture {
         shadowTexture.enableBlending()
     }
 
-    val background = texture.getSprite("background")
-    val backgroundInnerCorners = texture.getSprite("background.inner_corners")
-    val backgroundSlightRound = texture.getSprite("background.slight.round")
-    val backgroundSlightRoundInnerCorners = texture.getSprite("background.slight.round.inner_corners")
-    val backgroundSlight = texture.getSprite("background.slight")
-    val backgroundSlightInnerCorners = texture.getSprite("background.slight.inner_corners")
-    val backgroundSlightInset = texture.getSprite("background.slight.inset")
-    val backgroundSlightInsetInnerCorners = texture.getSprite("background.slight.inset.inner_corners")
-    val backgroundBlack = texture.getSprite("background.black")
-    val backgroundBlackInnerCorners = texture.getSprite("background.black.inner_corners")
+    val background: Sprite by texture.sprites
+    val backgroundInnerCorners: Sprite by texture.sprites
+    val backgroundSlightRound: Sprite by texture.sprites
+    val backgroundSlightRoundInnerCorners: Sprite by texture.sprites
+    val backgroundSlight: Sprite by texture.sprites
+    val backgroundSlightInnerCorners: Sprite by texture.sprites
+    val backgroundSlightInset: Sprite by texture.sprites
+    val backgroundSlightInsetInnerCorners: Sprite by texture.sprites
+    val backgroundBlack: Sprite by texture.sprites
+    val backgroundBlackInnerCorners: Sprite by texture.sprites
 
-    val windowBackgroundTitlebar = texture.getSprite("window.background.titlebar")
-    val windowBackgroundBody = texture.getSprite("window.background.body")
-    val windowDialogBackgroundTitlebar = texture.getSprite("window.dialog.background.titlebar")
-    val windowDialogBackgroundBody = texture.getSprite("window.dialog.background.body")
-    val windowSlightBackgroundTitlebar = texture.getSprite("window.slight.background.titlebar")
-    val windowSlightBackgroundBody = texture.getSprite("window.slight.background.body")
+    val windowBackgroundTitlebar: Sprite by texture.sprites
+    val windowBackgroundBody: Sprite by texture.sprites
+    val windowDialogBackgroundTitlebar: Sprite by texture.sprites
+    val windowDialogBackgroundBody: Sprite by texture.sprites
+    val windowSlightBackgroundTitlebar: Sprite by texture.sprites
+    val windowSlightBackgroundBody: Sprite by texture.sprites
 
-    val windowIconsCloseMacosSmall = texture.getSprite("window.icons.close.macos.small")
-    val windowIconsMinimizeMacosSmall = texture.getSprite("window.icons.minimize.macos.small")
-    val windowIconsMaximizeMacosSmall = texture.getSprite("window.icons.maximize.macos.small")
-    val windowIconsCloseMacos = texture.getSprite("window.icons.close.macos")
-    val windowIconsMinimizeMacos = texture.getSprite("window.icons.minimize.macos")
-    val windowIconsMaximizeMacos = texture.getSprite("window.icons.maximize.macos")
-    val windowIconsMinimize = texture.getSprite("window.icons.minimize")
-    val windowIconsMaximize = texture.getSprite("window.icons.maximize")
-    val windowIconsClose = texture.getSprite("window.icons.close")
+    val windowIconsCloseMacosSmall: Sprite by texture.sprites
+    val windowIconsMinimizeMacosSmall: Sprite by texture.sprites
+    val windowIconsMaximizeMacosSmall: Sprite by texture.sprites
+    val windowIconsCloseMacos: Sprite by texture.sprites
+    val windowIconsMinimizeMacos: Sprite by texture.sprites
+    val windowIconsMaximizeMacos: Sprite by texture.sprites
+    val windowIconsMinimize: Sprite by texture.sprites
+    val windowIconsMaximize: Sprite by texture.sprites
+    val windowIconsClose: Sprite by texture.sprites
 
-    val tabsButton = texture.getSprite("tabs.button")
-    val tabsBody = texture.getSprite("tabs.body")
-    val tabsButtonPressed = texture.getSprite("tabs.button.pressed")
-    val splitpaneRegion = texture.getSprite("splitpane.region")
-    val splitpaneHsplit = texture.getSprite("splitpane.hsplit")
-    val splitpaneVsplit = texture.getSprite("splitpane.vsplit")
+    val tabsButton: Sprite by texture.sprites
+    val tabsBody: Sprite by texture.sprites
+    val tabsButtonPressed: Sprite by texture.sprites
+    val splitpaneRegion: Sprite by texture.sprites
+    val splitpaneHsplit: Sprite by texture.sprites
+    val splitpaneVsplit: Sprite by texture.sprites
 
 
-    val textfield = texture.getSprite("textfield")
-    val dropdown = texture.getSprite("dropdown")
-    val dropdownBackground = texture.getSprite("dropdown.background")
-    val dropdownHighlight = texture.getSprite("dropdown.highlight")
-    val dropdownSeparator = texture.getSprite("dropdown.separator")
+    val textfield: Sprite by texture.sprites
+    val dropdown: Sprite by texture.sprites
+    val dropdownBackground: Sprite by texture.sprites
+    val dropdownHighlight: Sprite by texture.sprites
+    val dropdownSeparator: Sprite by texture.sprites
 
-    val scrollbarHandleHorizontal = texture.getSprite("scrollbar.handle.horizontal")
-    val scrollbarHandleVertical = texture.getSprite("scrollbar.handle.vertical")
-    val scrollbarTrack = texture.getSprite("scrollbar.track")
+    val scrollbarHandleHorizontal: Sprite by texture.sprites
+    val scrollbarHandleHorizontalDashes: Sprite by texture.sprites
+    val scrollbarHandleVertical: Sprite by texture.sprites
+    val scrollbarHandleVerticalDashes: Sprite by texture.sprites
+    val scrollbarTrack: Sprite by texture.sprites
 
-    val button = texture.getSprite("button")
-    val buttonPressed = texture.getSprite("button.pressed")
-    val switchOff = texture.getSprite("switch.off")
-    val switchOn = texture.getSprite("switch.on")
-    val switchHandle = texture.getSprite("switch.handle")
-    val radioButton = texture.getSprite("radio_button")
-    val checkbox = texture.getSprite("checkbox")
-    val progressbar = texture.getSprite("progressbar")
-    val progressbarFill = texture.getSprite("progressbar.fill")
-    val sliderHandle = texture.getSprite("slider.handle")
-    val sliderHandleRight = texture.getSprite("slider.handle.right")
-    val sliderHandleLeft = texture.getSprite("slider.handle.left")
-    val sliderHandleDown = texture.getSprite("slider.handle.down")
-    val sliderHandleUp = texture.getSprite("slider.handle.up")
+    val button: Sprite by texture.sprites
+    val buttonPressed: Sprite by texture.sprites
+    val switchOff: Sprite by texture.sprites
+    val switchOn: Sprite by texture.sprites
+    val switchHandle: Sprite by texture.sprites
+    val radioButton: Sprite by texture.sprites
+    val checkbox: Sprite by texture.sprites
+    val progressbar: Sprite by texture.sprites
+    val progressbarFill: Sprite by texture.sprites
+    val sliderHandle: Sprite by texture.sprites
+    val sliderHandleRight: Sprite by texture.sprites
+    val sliderHandleLeft: Sprite by texture.sprites
+    val sliderHandleDown: Sprite by texture.sprites
+    val sliderHandleUp: Sprite by texture.sprites
 
-    val shadowColor = texture.getColor("window.shadow.color")
-    val sliderLinesColor = texture.getColor("slider.lines")
-    val sliderLinesHighlightedColor = texture.getColor("slider.lines.highlighted")
+    val windowShadowColor: Color by texture.colors
+    val sliderLinesColor: Color by texture.colors
+    val sliderLinesHighlightedColor: Color by texture.colors
 
     val shadowFadeSize = 48
     val shadowSprite = shadowTexture.getSprite("shadow")
--- src/main/java/com/teamwizardry/librarianlib/features/facade/provided/pastry/components/PastryScrollPane.kt
+++ src/main/java/com/teamwizardry/librarianlib/features/facade/provided/pastry/components/PastryScrollPane.kt
@@ -25,232 +25,83 @@ import kotlin.math.roundToInt
 class PastryScrollPane(x: Int, y: Int, width: Int, height: Int): GuiComponent(x, y, width, height) {
     constructor(x: Int, y: Int): this(x, y, 0, 0)
     constructor(): this(0, 0, 0, 0)
-    /**
-     * The contents of the scroll pane. If the this component is larger than the pane the pane it will be able to
-     * scroll.
-     */
-    val content: GuiComponent = GuiComponent()
 
-    val verticalScrollBar: PastryScrollBar = PastryScrollBar(this, Axis2d.Y)
-    val horizontalScrollBar: PastryScrollBar = PastryScrollBar(this, Axis2d.X)
+    private val scrollPane = ScrollPane()
 
-    internal val viewport: GuiComponent = GuiComponent()
+    val content: GuiComponent = scrollPane.content
+
     private val background = PastryBackground(BackgroundTexture.SLIGHT_INSET)
+    private val verticalBackground = SpriteLayer(PastryTexture.scrollbarTrack)
+    private val horizontalBackground = SpriteLayer(PastryTexture.scrollbarTrack)
 
-    /**
-     * If false the scrollbars will be removed from this component and their layout will not be automatically managed.
-     * This is to allow their locations and sizes can be customized.
-     */
-    var embedScrollbars: Boolean = true
-        set(value) {
-            field = value
-            if(value) {
-                if(verticalScrollBar.parent != this) {
-                    verticalScrollBar.removeFromParent()
-                    this.add(verticalScrollBar)
-                }
-                if(horizontalScrollBar.parent != this) {
-                    horizontalScrollBar.removeFromParent()
-                    this.add(horizontalScrollBar)
-                }
-            } else {
-                if(verticalScrollBar.parent == this) {
-                    verticalScrollBar.removeFromParent()
-                }
-                if(horizontalScrollBar.parent == this) {
-                    horizontalScrollBar.removeFromParent()
-                }
-            }
-        }
-
-    /**
-     * Whether to use the default pastry textures. If set to false all the default pastry sprites will be removed to
-     * allow for custom rendering.
-     */
-    var usePastryTextures: Boolean = true
-
-    private var previousContentSize: Vec2d = vec(Double.NaN, Double.NaN)
+    private val verticalHandleBackground = SpriteLayer(PastryTexture.scrollbarHandleVertical)
+    private val horizontalHandleBackground = SpriteLayer(PastryTexture.scrollbarHandleHorizontal)
+    private val verticalHandleDashes = SpriteLayer(PastryTexture.scrollbarHandleVerticalDashes)
+    private val horizontalHandleDashes = SpriteLayer(PastryTexture.scrollbarHandleHorizontalDashes)
 
     init {
-        this.add(background, viewport, verticalScrollBar, horizontalScrollBar)
-        viewport.add(content)
-        viewport.clipToBounds = true
-        background.isVisible_im {
-            usePastryTextures
-        }
-        verticalScrollBar.widthi = 12
-        horizontalScrollBar.heighti = 12
+        this.add(background, horizontalBackground, verticalBackground)
+        this.add(scrollPane, scrollPane.verticalScrollBar, scrollPane.horizontalScrollBar)
+
+        scrollPane.verticalScrollBar.handle.add(verticalHandleBackground, verticalHandleDashes)
+        scrollPane.horizontalScrollBar.handle.add(horizontalHandleBackground, horizontalHandleDashes)
+        verticalHandleDashes.anchor = vec(0.5, 0.5)
+        horizontalHandleDashes.anchor = vec(0.5, 0.5)
     }
 
-    @Hook
-    private fun preFrame(e: GuiLayerEvents.PreFrameEvent) {
-        val newSize = content.frame.size
-        if(newSize != previousContentSize) {
-            previousContentSize = newSize
-            this.setNeedsLayout()
-        }
+    init {
+        scrollPane.verticalScrollBar.handle.BUS.hook(::layoutVerticalHandle)
+        scrollPane.horizontalScrollBar.handle.BUS.hook(::layoutHorizontalHandle)
+        scrollPane.BUS.hook<GuiLayerEvents.LayoutChildren> { layoutChildren() }
+    }
+
+    private fun layoutVerticalHandle(e: GuiLayerEvents.LayoutChildren) {
+        val handle = scrollPane.verticalScrollBar.handle
+        verticalHandleBackground.frame = handle.bounds
+
+        val dashHeight = ((handle.heighti-4)/3).clamp(0, 5) * 3
+        verticalHandleDashes.frame = rect(1, (handle.height - dashHeight) / 2, handle.width - 2, dashHeight)
+    }
+
+    private fun layoutHorizontalHandle(e: GuiLayerEvents.LayoutChildren) {
+        val handle = scrollPane.horizontalScrollBar.handle
+        horizontalHandleBackground.frame = handle.bounds
+
+        val dashWidth = ((handle.widthi-4)/3).clamp(0, 5) * 3
+        horizontalHandleDashes.frame = rect((handle.width - dashWidth) / 2, 1, dashWidth, handle.height - 2)
     }
 
     override fun layoutChildren() {
         super.layoutChildren()
-        if(usePastryTextures)
-            viewport.frame = this.bounds.shrink(1.0)
-        else
-            viewport.frame = this.bounds
-        if(embedScrollbars) {
-            verticalScrollBar.updateVisibility()
-            if (verticalScrollBar.isVisible)
-                viewport.frame = viewport.frame.offset(0, 0, -verticalScrollBar.frame.width, 0)
 
-            horizontalScrollBar.updateVisibility()
-            if (horizontalScrollBar.isVisible)
-                viewport.frame = viewport.frame.offset(0, 0, 0, -horizontalScrollBar.frame.height)
+        val scrollBarWidth = 12
 
+        val contentSize = content.frame.size
 
-            val viewportFrame = viewport.frame
-            verticalScrollBar.frame = rect(viewportFrame.maxX, 0, verticalScrollBar.frame.width, this.height)
-            horizontalScrollBar.frame = rect(0, viewportFrame.maxY, this.width, horizontalScrollBar.frame.height)
+        var needsVertical = contentSize.y > this.height - 2
+        var needsHorizontal = contentSize.x > this.width - 2
 
-            verticalScrollBar.runLayout()
-            horizontalScrollBar.runLayout()
+        if(needsHorizontal)
+            needsVertical = contentSize.y > this.height - 2 - scrollBarWidth
+        else if(needsVertical)
+            needsHorizontal = contentSize.x > this.width - 2 - scrollBarWidth
 
-            verticalScrollBar.updateHandle()
-            horizontalScrollBar.updateHandle()
-        } else {
-            verticalScrollBar.updateVisibility()
-            horizontalScrollBar.updateVisibility()
-        }
-        background.frame = viewport.frame.grow(1.0)
+        val contentAreaSize = vec(
+            this.width - if(needsVertical) scrollBarWidth else 0,
+            this.height - if(needsHorizontal) scrollBarWidth else 0
+        )
+
+        background.frame = rect(0, 0, contentAreaSize.x, contentAreaSize.y)
+        scrollPane.frame = background.frame.shrink(1.0)
+
+        scrollPane.verticalScrollBar.isVisible = needsVertical
+        verticalBackground.isVisible = needsVertical
+        scrollPane.horizontalScrollBar.isVisible = needsHorizontal
+        horizontalBackground.isVisible = needsHorizontal
+
+        verticalBackground.frame = rect(this.width - scrollBarWidth, 0, scrollBarWidth, contentAreaSize.y)
+        scrollPane.verticalScrollBar.frame = verticalBackground.frame.shrink(1.0)
+        horizontalBackground.frame = rect(0, this.height - scrollBarWidth, contentAreaSize.x, scrollBarWidth)
+        scrollPane.horizontalScrollBar.frame = horizontalBackground.frame.shrink(1.0)
     }
-}
-
-class PastryScrollBar internal constructor(private val scrollPane: PastryScrollPane, val axis: Axis2d): GuiComponent() {
-    /**
-     * The actual handle that will be dragged
-     */
-    val handle: GuiComponent = GuiComponent()
-
-    /**
-     * Whether to show the scrollbar. If this value is null the scrollbar will be automatically shown or hidden based
-     * on whether the content is larger than the viewport.
-     */
-    val scrollbarVisibility: Boolean? = null
-
-    /**
-     * Whether to resize the handle based on the scrollable distance. (e.g. if the content is only slightly larger than
-     * the viewport the handle will be almost the same height as the entire scrollbar)
-     */
-    var resizeHandle: Boolean = true
-
-    /**
-     * This is used instead of an IMValue callback as the latter can easily be obliterated accidentally.
-     */
-    internal fun updateVisibility() {
-        scrollbarVisibility?.also {
-            this.isVisible = it
-            return
-        }
-
-        val viewportSize = scrollPane.viewport.bounds.size[axis]
-        val contentSize = scrollPane.content.frame.size[axis]
-        this.isVisible = viewportSize < contentSize
-    }
-
-    /**
-     * The offset for the scroll pane contents. This is stored in actual offset pixels both for convenience and so the
-     * scroll position is retained even if the contents change in size. A positive scroll position indicates that the
-     * contents should be moved in the negative direction, so this acts like moving the viewport over a static content
-     * component. Negative scroll positions are clamped to zero
-     */
-    var scrollPosition: Double
-        get() = _scrollPosition
-        set(value) {
-            _scrollPosition = value
-            updateHandle()
-        }
-
-    private val background = SpriteLayer(PastryTexture.scrollbarTrack)
-    private val handleImage = SpriteLayer(
-        if(axis == Axis2d.X) PastryTexture.scrollbarHandleHorizontal else PastryTexture.scrollbarHandleVertical
-    )
-
-    private var _scrollPosition: Double = 0.0
-        set(value) {
-            field = value
-            scrollPane.content.pos = scrollPane.content.pos.setAxis(axis, -value.roundToInt().toDouble())
-        }
-
-    private var dragPosition: Double? = null
-
-    init {
-        background.isVisible_im { scrollPane.usePastryTextures }
-        handleImage.isVisible_im { scrollPane.usePastryTextures }
-        this.add(background, handle)
-        handle.add(handleImage)
-
-        handle.BUS.hook<GuiComponentEvents.MouseDownEvent> {
-            if(handle.mouseOver)
-                dragPosition = if(handle.size[axis] == 0.0) 0.0 else handle.mousePos[axis] / handle.size[axis]
-        }
-
-        handle.BUS.hook<GuiComponentEvents.MouseMoveEvent> {
-            updateHandle()
-        }
-
-        handle.BUS.hook<GuiComponentEvents.MouseUpEvent> {
-            updateHandle()
-            dragPosition = null
-        }
-    }
-
-    override fun layoutChildren() {
-        background.frame = this.bounds
-        val viewportSize = scrollPane.viewport.size[axis]
-        val contentSize = scrollPane.content.size[axis]
-        var handleSize = handle.size[axis]
-        if(resizeHandle) {
-            handleSize = (
-                (if(contentSize == .0) 1.0 else min(1.0, viewportSize / contentSize)) * this.size[axis]
-                ).roundToInt().toDouble()
-            if(scrollPane.usePastryTextures) {
-                // The pastry handle should always be 3 + 2n
-                handleSize = 3.0 + (handleSize - 3).toInt() / 2 * 2
-            }
-            handleSize = this.BUS.fire(ResizeHandleEvent(handleSize)).size
-        }
-
-        if(scrollPane.usePastryTextures) {
-            handle.size = (this.size - vec(2, 2)).setAxis(axis, handleSize)
-        } else {
-            handle.size = this.size.setAxis(axis, handleSize)
-        }
-        handleImage.frame = handle.bounds
-        updateHandle()
-    }
-
-    internal fun updateHandle() {
-        val viewportSize = scrollPane.viewport.size[axis]
-        val contentSize = scrollPane.content.size[axis]
-        val neededScroll = max(0.0, contentSize - viewportSize)
-
-        val handleScrollSpace = this.size[axis] - handle.size[axis]
-
-        val pastryInset = if(scrollPane.usePastryTextures) 1.0 else 0.0
-        val dragPosition = dragPosition
-        if(dragPosition != null) {
-            var handlePos = mousePos[axis] - handle.size[axis] * dragPosition
-            handlePos = handlePos.clamp(0.0, handleScrollSpace)
-            handle.pos = vec(pastryInset, pastryInset).setAxis(axis, handlePos)
-            _scrollPosition = neededScroll * if(handleScrollSpace == 0.0) 0.0 else handlePos / handleScrollSpace
-        } else {
-            var scrollFraction = if(neededScroll == 0.0) 0.0 else scrollPosition / neededScroll
-            if(scrollFraction > 1) {
-                scrollFraction = 1.0
-                _scrollPosition = neededScroll
-            }
-            handle.pos = vec(pastryInset, pastryInset).setAxis(axis, handleScrollSpace * scrollFraction)
-        }
-    }
-
-    data class ResizeHandleEvent(var size: Double): Event()
 }
