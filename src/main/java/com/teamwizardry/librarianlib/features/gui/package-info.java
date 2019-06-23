ae9fdccff2c688c8a4b23e80444cb5b319ddd730
Pierce Corcoran <pierce@plasticcow.com>
2019-06-23 13:11:01 -0700

    Fix package deprecation annotation errors

--- src/main/java/com/teamwizardry/librarianlib/features/gui/package-info.java
+++ src/main/java/com/teamwizardry/librarianlib/features/gui/package-info.java
@@ -2,5 +2,4 @@
  * @deprecated As of version 4.20 this has been superseded by
  * {@link com.teamwizardry.librarianlib.features.facade Facade}
  */
-@Deprecated
 package com.teamwizardry.librarianlib.features.gui;
