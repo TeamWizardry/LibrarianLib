1c2887c6b6486faff147609de0e2c84a2621368d
Pierce Corcoran <pierce@plasticcow.com>
2019-06-24 20:38:14 -0700

    Convert mod to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,164 @@
+@file:Suppress("PropertyName")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.time.format.DateTimeFormatter
+import java.time.LocalDateTime
+import java.time.ZoneId
+import kotlin.concurrent.thread
+import kotlin.reflect.KProperty
+
+plugins {
+    idea
+    eclipse
+    `maven-publish`
+    java
+    kotlin("jvm")
+    id("net.minecraftforge.gradle")
+}
+
+val mc_version: String by gradleProperties
+val forge_version: String by gradleProperties
+val mc_mappings: String by gradleProperties
+
+val branch: String = gradleProperties["branch"] ?: "git rev-parse --abbrev-ref HEAD".execute(rootDir.absolutePath).lines().last()
+logger.info("On branch $branch")
+
+val mod_version_prefix = if(mc_version.contains(branch)) "" else "${branch.replace('/', '-')}-"
+
+// ===================================================== Common ===================================================== //
+
+allprojects {
+    val mod_version: String by gradleProperties
+    val mod_name: String by gradleProperties
+    val mod_group: String by gradleProperties
+
+    group = mod_group
+    version = mod_version_prefix + mod_version
+    description = "A library for the TeamWizardry mods "
+    if(project == rootProject)
+        base.archivesBaseName = "$mod_name-$mod_version_prefix$mc_version"
+    else
+        base.archivesBaseName = "librarianlib-$mod_name"
+
+    minecraft {
+        mappings = "$mc_mappings-$mc_version"
+        // accessTransformer = file("build/resources/main/META-INF/accesstransformer.cfg")
+//        runDir = "run"
+//        coreMod = core_plugin
+
+//        replace("GRADLE:VERSION", mod_version)
+//        replace("GRADLE:BUILD", build_number)
+//        replaceIn("LibrarianLib.kt")
+    }
+
+    repositories {
+        maven(url = "http://maven.shadowfacts.net/")
+    }
+
+    dependencies {
+        minecraft("net.minecraftforge:forge:$mc_version-$forge_version")
+        compile(kotlin("stdlib-jdk8"))
+    }
+
+    tasks.getByName<Jar>("jar") {
+        manifest {
+//            attributes(mapOf(
+//                "Specification-Title" to mod_name,
+//                "Specification-Vendor" to "Team Wizardry",
+//                "Specification-Version" to version,
+//                "Implementation-Title" to project.name,
+//                "Implementation-Version" to version,
+//                "Implementation-Vendor" to "examplemodsareus",
+//                "Implementation-Timestamp" to DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss").format(LocalDateTime.now(ZoneId.of("UTC")))
+//            ))
+        }
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            javaParameters = true
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-Xjvm-default=enable",
+                "-Xuse-experimental=kotlin.Experimental"
+            )
+        }
+    }
+}
+
+// ====================================================== Root ====================================================== //
+
+minecraft {
+    runs {
+        "client" {
+            workingDirectory(project.file("run"))
+
+            // Recommended logging data for a userdev environment
+//            property("forge.logging.markers", "SCAN,REGISTRIES,REGISTRYDUMP")
+
+            // Recommended logging level for the console
+            property("forge.logging.console.level", "debug")
+
+            mods {
+                "librarianlib" {
+                    source(java.sourceSets["main"])
+                }
+            }
+        }
+
+        "server" {
+            workingDirectory(project.file("run"))
+
+            // Recommended logging data for a userdev environment
+//            property("forge.logging.markers", "SCAN,REGISTRIES,REGISTRYDUMP")
+
+            // Recommended logging level for the console
+            property("forge.logging.console.level", "debug")
+
+            mods {
+                "librarianlib" {
+                    source(java.sourceSets["main"])
+                }
+            }
+        }
+    }
+}
+
+// ==================================================== Utilities =================================================== //
+
+fun String.execute(wd: String? = null, ignoreExitCode: Boolean = false): String =
+    split(" ").execute(wd, ignoreExitCode)
+
+fun List<String>.execute(wd: String? = null, ignoreExitCode: Boolean = false): String {
+    val process = ProcessBuilder(this)
+        .also { pb -> wd?.let { pb.directory(File(it)) } }
+        .start()
+    var result = ""
+    val errReader = thread { process.errorStream.bufferedReader().forEachLine { logger.error(it) } }
+    val outReader = thread {
+        process.inputStream.bufferedReader().forEachLine { line ->
+            logger.debug(line)
+            result += line
+        }
+    }
+    process.waitFor()
+    outReader.join()
+    errReader.join()
+    if (process.exitValue() != 0 && !ignoreExitCode) error("Non-zero exit status for `$this`")
+    return result
+}
+
+val Project.gradleProperties: ProjectPropertiesDelegate
+    get() = ProjectPropertiesDelegate(this)
+
+@Suppress("UNCHECKED_CAST")
+class ProjectPropertiesDelegate(private val project: Project) {
+    operator fun <T> getValue(receiver: Any?, property: KProperty<*>): T = this[property.name]
+    operator fun <T> get(name: String): T = project.extra.properties[name] as T
+    operator fun contains(name: String): Boolean = project.extra.properties[name] != null
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,22 @@
+val kotlin_version: String by settings
+val forgegradle_version: String by settings
 
+pluginManagement {
+    repositories {
+        maven {
+            name = "forge"
+            url = uri("https://files.minecraftforge.net/maven")
+        }
+        gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("org.jetbrains.kotlin")) useVersion(kotlin_version)
+            else when (requested.id.id) {
+                "net.minecraftforge.gradle" -> useModule("net.minecraftforge.gradle:ForgeGradle:$forgegradle_version")
+            }
+        }
+    }
+}
+
+rootProject.name = "librarianlib"
