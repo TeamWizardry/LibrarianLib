30b3c7dae2c1b91dea3cfe9853c8a28d0f9da13d
Pierce Corcoran <pierce@plasticcow.com>
2019-08-23 11:07:50 -0700

    Slightly refactor module class

--- modules/core/src/main/java/com/teamwizardry/librarianlib/core/LibrarianLibModule.kt
+++ modules/core/src/main/java/com/teamwizardry/librarianlib/core/LibrarianLibModule.kt
@@ -1,10 +1,36 @@
 package com.teamwizardry.librarianlib.core
 
 import com.teamwizardry.librarianlib.core.util.kotlin.unmodifiableView
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.api.distmarker.OnlyIn
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import org.apache.logging.log4j.Logger
 
-abstract class LibrarianLibModule(val modid: String) {
+abstract class LibrarianLibModule(val name: String, val logger: Logger) {
+    val modid: String = "librarianlib-$name"
+
     init {
         register(modid, this)
+
+        FMLJavaModLoadingContext.get().modEventBus.addListener<FMLCommonSetupEvent> {
+            this.setup(it)
+        }
+        FMLJavaModLoadingContext.get().modEventBus.addListener<FMLClientSetupEvent> {
+            this.clientSetup(it)
+        }
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this)
+    }
+
+    protected fun setup(event: FMLCommonSetupEvent) {
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    protected fun clientSetup(event: FMLClientSetupEvent) {
     }
 
     companion object {
