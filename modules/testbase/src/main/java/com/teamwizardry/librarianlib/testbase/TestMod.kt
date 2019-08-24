8e2823c7b61d7a587feeed35796d77f251218bb9
Pierce Corcoran <pierce@plasticcow.com>
2019-08-23 23:59:22 -0700

    Add colored band to test item

--- modules/testbase/src/main/java/com/teamwizardry/librarianlib/testbase/TestMod.kt
+++ modules/testbase/src/main/java/com/teamwizardry/librarianlib/testbase/TestMod.kt
@@ -1,21 +1,28 @@
 package com.teamwizardry.librarianlib.testbase
 
 import com.teamwizardry.librarianlib.core.LibrarianLibModule
+import com.teamwizardry.librarianlib.core.util.Client
 import com.teamwizardry.librarianlib.core.util.kotlin.unmodifiableView
+import com.teamwizardry.librarianlib.testbase.objects.TestItem
 import com.teamwizardry.librarianlib.testbase.objects.TestObject
 import net.minecraft.block.Block
+import net.minecraft.client.renderer.color.IItemColor
+import net.minecraft.client.renderer.color.ItemColors
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraftforge.client.event.ColorHandlerEvent
 import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.client.registry.RenderingRegistry
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
 import net.minecraftforge.registries.ForgeRegistries
 import org.apache.logging.log4j.Logger
+import java.awt.Color
 
 abstract class TestMod(name: String, logger: Logger): LibrarianLibModule("$name-test", logger) {
 
@@ -55,6 +62,16 @@ abstract class TestMod(name: String, logger: Logger): LibrarianLibModule("$name-
         entities.forEach {
 //            RenderingRegistry.registerEntityRenderingHandler(entityClass) { ParticleSpawnerEntityRenderer(it) }
         }
+    }
+
+    @SubscribeEvent
+    internal fun registerColors(colorHandlerEvent: ColorHandlerEvent.Item) {
+        colorHandlerEvent.itemColors.register(IItemColor { stack, tintIndex ->
+            if(tintIndex == 1 && stack.item is TestItem)
+                Color.getHSBColor((stack.item.registryName.hashCode() / 1000.toFloat()) % 1, 0.8f, 0.8f).rgb
+            else
+                Color.WHITE.rgb
+        }, *items.toTypedArray())
     }
 
     override fun registerBlocks(blockRegistryEvent: RegistryEvent.Register<Block>) {
