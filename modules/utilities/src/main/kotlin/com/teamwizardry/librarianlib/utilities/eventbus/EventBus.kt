cc5f0c2dc56cbd3b6feeda6b3c5dba16db1056dd
Pierce Corcoran <pierce@plasticcow.com>
2020-01-19 11:06:58 -0800

    Add event system

---
+++ modules/utilities/src/main/kotlin/com/teamwizardry/librarianlib/utilities/eventbus/EventBus.kt
@@ -0,0 +1,93 @@
+package com.teamwizardry.librarianlib.utilities.eventbus
+
+import java.util.function.Consumer
+
+class EventBus {
+    private var hooks = mutableMapOf<Class<*>, MutableList<EventHook>>()
+
+    /**
+     * Causes this event bus to delegate all its event handling to the given bus. This effectively makes the two busses
+     * identical views of each other
+     */
+    fun delegateTo(other: EventBus) {
+        hooks.forEach { clazz, hooks ->
+            other.hooks.getOrPut(clazz) { mutableListOf() }.addAll(hooks)
+        }
+        hooks = other.hooks
+    }
+
+    fun hasHooks(clazz: Class<*>): Boolean {
+        return hooks[clazz]?.size ?: 0 > 0
+    }
+
+    fun <E : Event> fire(event: E): E {
+        getEventClassList(event.javaClass).forEach { clazz ->
+            fire(event, clazz)
+        }
+        return event
+    }
+
+    private fun fire(event: Event, clazz: Class<*>) {
+        if (clazz in hooks) {
+            if (event.reversed)
+                hooks[clazz]?.asReversed()?.forEach {
+                    it.fire(event)
+                }
+            else
+                hooks[clazz]?.forEach {
+                    it.fire(event)
+                }
+        }
+    }
+
+    inline fun <reified  E : Event> hook(noinline hook: (E) -> Unit) {
+        hook(E::class.java, hook)
+    }
+
+    fun <E : Event> hook(clazz: Class<E>, hook: (E) -> Unit) {
+        hook(clazz, Consumer(hook))
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <E : Event> hook(clazz: Class<E>, hook: Consumer<E>) {
+        if (!hooks.containsKey(clazz))
+            hooks.put(clazz, mutableListOf())
+        hooks[clazz]?.add(EventHook(hook as Consumer<Event>))
+    }
+
+    fun register(obj: Any) {
+        EventHookAnnotationReflector.apply(this, obj)
+    }
+
+    private class EventHook(val callback: Consumer<Event>) {
+        var data: Any? = null
+
+        fun fire(event: Event) {
+            event.hookDataInternal = data
+            event.initializeHookStateInternal()
+            callback.accept(event)
+            event.finalizeHookStateInternal()
+            data = event.hookDataInternal
+            event.hookDataInternal = null
+        }
+    }
+
+    companion object {
+        private val classLists = mutableMapOf<Class<*>, List<Class<*>>>()
+
+        private fun getEventClassList(clazz: Class<*>): Iterable<Class<*>> {
+            return classLists.getOrPut(clazz) {
+                val list = mutableListOf<Class<*>>()
+
+                var c = clazz
+                while(Event::class.java.isAssignableFrom(c)) {
+                    list.add(c)
+                    c = c.superclass ?: break
+                }
+
+                list
+            }
+        }
+    }
+}
+
---
+++ modules/utilities/src/main/kotlin/com/teamwizardry/librarianlib/utilities/eventbus/EventHookAnnotation.kt
@@ -0,0 +1,49 @@
+package com.teamwizardry.librarianlib.utilities.eventbus
+
+import com.teamwizardry.librarianlib.core.util.kotlin.IS_DEOBFUSCATED
+import dev.thecodewarrior.mirror.Mirror
+import dev.thecodewarrior.mirror.member.MethodMirror
+import dev.thecodewarrior.mirror.type.TypeMirror
+import java.lang.reflect.Method
+
+/**
+ * Annotate methods with this, giving them a single parameter that inherits from [Event]. Passing this class to
+ * [EventBus.register] will then register each method with its parameter type. This is automatically called for
+ * [GuiLayer]s
+ */
+@Target(AnnotationTarget.FUNCTION)
+annotation class Hook
+
+internal object EventHookAnnotationReflector {
+    val cache = mutableMapOf<Class<*>, EventCache>()
+    private var nextClear = System.currentTimeMillis()
+    private val clearInterval = 10_000
+
+    fun apply(bus: EventBus, obj: Any) {
+
+        // clear cache periodically to support hotswapped classes
+        if(IS_DEOBFUSCATED && System.currentTimeMillis() > nextClear) {
+            nextClear = System.currentTimeMillis() + clearInterval
+            cache.clear()
+        }
+
+        cache.getOrPut(obj.javaClass) { EventCache(obj.javaClass) }.events.forEach { (type, method) ->
+            @Suppress("UNCHECKED_CAST")
+            bus.hook(type.erasure as Class<Event>) { event: Event ->
+                method(obj, event)
+            }
+        }
+    }
+
+    class EventCache(clazz: Class<*>) {
+        val events: List<Pair<TypeMirror, MethodMirror>>
+
+        init {
+            val mirror = Mirror.reflectClass(clazz)
+            events = mirror.allMethods
+                .filter { it.annotations.any { it is Hook } }
+                .filter { it.parameters.size == 1 && it.parameterTypes[0].isAssignableFrom(Mirror.reflectClass<Event>()) }
+                .map { it.parameterTypes[0] to it }
+        }
+    }
+}
